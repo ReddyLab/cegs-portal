b2b7814948f1af1168a2e084b41dbcfece52cec7
Thomas Cowart <thomas.cowart@duke.edu>
2021-08-27 16:45:31 -0400

    Replaces tachyon with tailwind CSS
    Tachyon was missing an important feature -- the ability to compose
    classes once a set of utility classes was settled on. The whole set
    had to be repeated over and over again.
    
    Tailwind solves this with a CSS pre-processor and the @apply directive.
    However, setting that up required creating a npm package and installing
    a bunch of node modules.
    
    However, now you can start a django dev server by running "npm run dev".
    This will start django and, separately, start the preprocessor in "watch"
    mode. The preprocessor will automatically regenerate the css when the
    css.tw file is changed.

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+    './cegs_portal/**/*.html',
+    './cegs_portal/**/*.js',
+  ]},
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
