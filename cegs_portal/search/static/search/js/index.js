44d48423ea8c290dcd832f9bb9069835a57db595
Thomas Cowart <thomas.cowart@duke.edu>
2021-10-21 10:04:43 -0400

    Prettify index.js

--- cegs_portal/search/static/search/js/index.js
+++ cegs_portal/search/static/search/js/index.js
@@ -1,29 +1,30 @@
 function e(name) {
-    attributes = {}
-    children = []
-    if(arguments.length == 2) {
-        children = arguments[1]
+    attributes = {};
+    children = [];
+    if (arguments.length == 2) {
+        children = arguments[1];
     }
-    if(arguments.length == 3) {
-        attributes = arguments[1]
-        children = arguments[2]
+    if (arguments.length == 3) {
+        attributes = arguments[1];
+        children = arguments[2];
     }
 
     element = document.createElement(name);
-    for(key in attributes) {
+    for (key in attributes) {
         element.setAttribute(key, attributes[key]);
     }
-    if(Array.isArray(children)) {
-        for(child of children) {
-            if(typeof child === "string") {
-                child = t(child)
+    if (Array.isArray(children)) {
+        for (child of children) {
+            if (typeof child === "string") {
+                child = t(child);
             }
             element.appendChild(child);
         }
     } else {
-        if(typeof children === "string") {
-            children = t(children)
+        if (typeof children === "string") {
+            children = t(children);
         }
+
         element.appendChild(children);
     }
 
