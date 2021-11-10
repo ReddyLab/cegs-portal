6a7f575b836aff1fc906e19cc177dda081396cb4
Thomas Cowart <thomas.cowart@duke.edu>
2021-11-10 16:49:48 -0500

    New query tokenizing algorithm
    The old algorithm, which split the query on spaces and then checked
    each part against the regexes had a downside that the regexes couldn't
    match strings. so, e.g.,  "chr1: 1-10" couldn't be matched even though
    we can easily write a regex for it.
    
    The new algorithm doesn't have this issue.

--- cegs_portal/search/view_models/search.py
+++ cegs_portal/search/view_models/search.py
@@ -10,40 +10,49 @@ from cegs_portal.search.models import (
 from cegs_portal.search.models.reg_effects import RegulatoryEffect
 from cegs_portal.search.models.utils import QueryToken
 
-CHROMO_RE = re.compile(r"(chr[12]?[123456789xym]):(\d+)(-(\d+))?", re.IGNORECASE)
-ENSEMBL_RE = re.compile(r"ENS[0-9a-z]+", re.IGNORECASE)
-HAVANA_RE = re.compile(r"OTT[0-9a-z]+", re.IGNORECASE)
-HUGO_RE = re.compile(r"HGNC:[0-9a-z]+", re.IGNORECASE)
-ASSEMBLY_RE = re.compile(r"hg19|hg38|grch37|grch38", re.IGNORECASE)
+CHROMO_RE = re.compile(r"\b((chr[12]?[123456789xym])\s*:\s*(\d+)(-(\d+))?)\b", re.IGNORECASE)
+ENSEMBL_RE = re.compile(r"\b(ENS[0-9a-z]+)", re.IGNORECASE)
+# HAVANA_RE = re.compile(r"\b(OTT[0-9a-z]+)", re.IGNORECASE)
+# HUGO_RE = re.compile(r"\b(HGNC:[0-9a-z]+)", re.IGNORECASE)
+ASSEMBLY_RE = re.compile(r"\b(hg19|hg38|grch37|grch38)\b", re.IGNORECASE)
+GENE_NAME_RE = re.compile(r"\b([A-Z0-9][A-Z0-9\.\-]+)\b", re.IGNORECASE)
 
 
-def parse_query(query: str) -> tuple[list[QueryToken], Optional[ChromosomeLocation], Optional[str]]:
+def parse_query(query: str) -> tuple[list[QueryToken], Optional[ChromosomeLocation], Optional[str], list[str]]:
     terms: list[tuple[QueryToken, str]] = []
     assembly = None
     location = None
-    tokens: list[str] = query.split()
+    gene_names = []
 
-    for token in tokens:
-        if result := CHROMO_RE.match(token):
-            location = ChromosomeLocation(result.group(1), result.group(2), result.group(4))
-        elif result := ENSEMBL_RE.match(token):
-            terms.append(QueryToken.ENSEMBL_ID.associate(token))
-        elif result := ASSEMBLY_RE.match(token):
-            token = token.lower()
+    for result in re.finditer(CHROMO_RE, query):
+        location = ChromosomeLocation(result.group(2), result.group(3), result.group(5))
+    query = re.sub(CHROMO_RE, " ", query)
 
-            # Normalize token
-            if token == "hg19" or token == "grch37":
-                token = "GRCh37"
-            elif token == "hg38" or token == "grch38":
-                token = "GRCh38"
-            assembly = token
-    return terms, location, assembly
+    for result in re.finditer(ENSEMBL_RE, query):
+        terms.append(QueryToken.ENSEMBL_ID.associate(result.group(1)))
+    query = re.sub(ENSEMBL_RE, " ", query)
+
+    for result in re.finditer(ASSEMBLY_RE, query):
+        token = result.group(1).lower()
+
+        # Normalize token
+        if token == "hg19" or token == "grch37":
+            token = "GRCh37"
+        elif token == "hg38" or token == "grch38":
+            token = "GRCh38"
+        assembly = token
+    query = re.sub(ASSEMBLY_RE, " ", query)
+
+    for result in re.finditer(GENE_NAME_RE, query):
+        gene_names.append(result.group(1))
+
+    return terms, location, assembly, gene_names
 
 
 class Search:
     @classmethod
     def search(cls, query_string):
-        query_terms, location, assembly_name = parse_query(query_string)
+        query_terms, location, assembly_name, gene_names = parse_query(query_string)
         feature_assembly_dict: dict[Feature, list[FeatureAssembly]] = {}
         targeting_effects_dict: dict[Feature, list[RegulatoryEffect]] = {}
         sites = None
