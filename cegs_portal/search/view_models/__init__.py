7e97dc112fe7d30e04690d2b026f15d9a365e040
Thomas Cowart <thomas.cowart@duke.edu>
2021-12-10 16:30:03 -0500

    Removes "genes" as an endpoint.
    Genes have been replaced by "features"

--- cegs_portal/search/view_models/__init__.py
+++ cegs_portal/search/view_models/__init__.py
@@ -1,6 +1,5 @@
 from .dhss import DHSSearch
 from .experiment import ExperimentSearch
-from .features import FeatureSearch
-from .genes import GeneSearch, IdSearchType, IdType, LocSearchType
+from .features import FeatureSearch, IdType
 from .reg_effects import RegEffectSearch
 from .search import Search
--- cegs_portal/search/views/__init__.py
+++ cegs_portal/search/views/__init__.py
@@ -1,7 +1,6 @@
 from .dhss import DHS, DHSLoc
 from .experiment import ExperimentView
 from .features import feature, feature_loc
-from .genes import gene, gene_loc
 from .index import index
 from .reg_effects import RegEffectView
 from .search import results
