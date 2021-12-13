831d80bc389d0d115790486626273513cb478894
Thomas Cowart <thomas.cowart@duke.edu>
2021-12-13 13:38:33 -0500

    Converts feature location search to use TemplateJsonView
    Also fixes some CSS and template issues with the search/features.html
    template feature location search uses.

--- cegs_portal/search/views/__init__.py
+++ cegs_portal/search/views/__init__.py
@@ -1,6 +1,6 @@
 from .dhss import DHS, DHSLoc
 from .experiment import ExperimentView
-from .features import feature, feature_loc
+from .features import FeatureLoc, feature
 from .index import index
 from .reg_effects import RegEffectView
 from .search import results
--- cegs_portal/search/views/features.py
+++ cegs_portal/search/views/features.py
@@ -2,6 +2,7 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 
 from cegs_portal.search.view_models import FeatureSearch, IdType
+from cegs_portal.search.views.custom_views import TemplateJsonView
 from cegs_portal.search.views.renderers import json
 from cegs_portal.search.views.view_utils import JSON_MIME
 
@@ -49,38 +50,51 @@ def feature(request, id_type, feature_id):
     return render(request, "search/features.html", results)
 
 
-# @method_decorator(csrf_exempt, name='dispatch') # only needed for POST, in dev.
-def feature_loc(request, chromo, start, end):
-    """
-    Headers used:
-        accept
-            * application/json
-    GET queries used:
-        accept
-            * application/json
-        format
-            * genoverse
-        search_type
-            * exact
-            * overlap
-        assembly
-            * free-text, but should match a genome assembly that exists in the DB
-    """
-    search_type = request.GET.get("search_type", "overlap")
-    assembly = request.GET.get("assembly", None)
-    response_format = request.GET.get("format", None)
-    feature_types = request.GET.getlist("feature", ["gene"])
+class FeatureLoc(TemplateJsonView):
+    template = "search/features.html"
 
-    if chromo.isnumeric():
-        chromo = f"chr{chromo}"
+    def request_options(self, request):
+        """
+        Headers used:
+            accept
+                * application/json
+        GET queries used:
+            accept
+                * application/json
+            format
+                * genoverse
+            search_type
+                * exact
+                * overlap
+            assembly
+                * free-text, but should match a genome assembly that exists in the DB
+        """
+        options = super().request_options(request)
+        options["search_type"] = request.GET.get("search_type", "overlap")
+        options["assembly"] = request.GET.get("assembly", None)
+        options["feature_types"] = request.GET.getlist("feature", ["gene"])
 
-    assemblies = FeatureSearch.loc_search(chromo, start, end, assembly, feature_types, search_type)
+        return options
 
-    results = {
-        "assemblies": list(assemblies),
-    }
+    def get_json(self, _request, options, data_handler, chromo, start, end):
+        results = [json(result, options["json_format"]) for result in data_handler(options, chromo, start, end)]
+        return JsonResponse(results, safe=False)
 
-    if request.headers.get("accept") == JSON_MIME or request.GET.get("accept", None) == JSON_MIME:
-        return JsonResponse([json(result, response_format) for result in results["assemblies"]], safe=False)
+    def get_template_prepare_data(self, data, options, chromo, start, end):
+        return {"features": data, "feature_name": "Genes"}
 
-    return render(request, "search/features.html", results)
+    def get_data(self, options, chromo, start, end):
+        if chromo.isnumeric():
+            chromo = f"chr{chromo}"
+
+        assemblies = FeatureSearch.loc_search(
+            chromo, start, end, options["assembly"], options["feature_types"], options["search_type"]
+        )
+
+        features = {}
+        for assembly in assemblies.all():
+            feature_list = features.get(assembly.feature, [])
+            feature_list.append(assembly)
+            features[assembly.feature] = feature_list
+
+        return features
