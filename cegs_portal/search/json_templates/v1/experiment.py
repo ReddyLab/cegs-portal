2bef01abfe32a8c04ed4f15d60a489d382bfb459
Thomas Cowart <thomas.cowart@duke.edu>
2022-01-31 12:47:03 -0500

    Handles dynamically added attrs better in experiment JSON Template
    Some attributes are added using setattr, this change handles them
    better (i.e., doesn't just assume they exist) when building JSON.

--- cegs_portal/search/json_templates/v1/experiment.py
+++ cegs_portal/search/json_templates/v1/experiment.py
@@ -4,30 +4,50 @@ from cegs_portal.search.models import Experiment, ExperimentDataFile, File
 
 
 def experiments(experiments_obj: Iterable[Experiment]):
-    return [
+    results = [
         {
             "id": e.id,
             "name": e.name,
             "description": e.description,
-            "cell_lines": e.cell_lines,  # type: ignore[attr-defined]
-            "tissue_types": e.tissue_types,  # type: ignore[attr-defined]
+            "cell_lines": None,
+            "tissue_types": None,
         }
         for e in experiments_obj
     ]
 
+    for e, result in zip(experiments_obj, results):
+        if hasattr(e, "cell_lines"):
+            result["cell_lines"] = [line for line in e.cell_lines]  # type: ignore[attr-defined]
+
+        if hasattr(e, "tissue_types"):
+            result["tissue_types"] = [tt for tt in e.tissue_types]  # type: ignore[attr-defined]
+
+    return results
+
 
 def experiment(experiment_obj: Experiment, json_format: str = None):
-    return {
+    result = {
         "id": experiment_obj.id,
         "name": experiment_obj.name,
         "description": experiment_obj.description,
         "assay": experiment_obj.experiment_type,
-        "cell_lines": [str(line) for line in experiment_obj.cell_lines],  # type: ignore[attr-defined]
-        "tissue_types": [str(tt) for tt in experiment_obj.tissue_types],  # type: ignore[attr-defined]
-        "assemblies": list(experiment_obj.assemblies),
+        "cell_lines": None,
+        "tissue_types": None,
+        "assemblies": None,
         "data_files": [data_file(f) for f in experiment_obj.data_files.all()],
         "other_files": [other_file(f) for f in experiment_obj.other_files.all()],
     }
+
+    if hasattr(experiment_obj, "cell_lines"):
+        result["cell_lines"] = [str(line) for line in experiment_obj.cell_lines]  # type: ignore[attr-defined]
+
+    if hasattr(experiment_obj, "tissue_types"):
+        result["tissue_types"] = [str(tt) for tt in experiment_obj.tissue_types]  # type: ignore[attr-defined]
+
+    if hasattr(experiment_obj, "assemblies"):
+        result["assemblies"] = list(experiment_obj.assemblies)  # type: ignore[attr-defined]
+
+    return result
 
 
 def data_file(data_file_obj: ExperimentDataFile):
