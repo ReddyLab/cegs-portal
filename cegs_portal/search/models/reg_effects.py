f3876bee3b5e6af632d8ada228985340907a514e
Thomas Cowart <thomas.cowart@duke.edu>
2022-03-08 14:51:41 -0500

    Add facet "properties" to RegEffect
    Adding properties to the RegulatoryEffect model will make using the facets
    easier and keeps us from having to re-write a lot of the
    view/view_model/json_template code.

--- cegs_portal/search/models/reg_effects.py
+++ cegs_portal/search/models/reg_effects.py
@@ -16,19 +16,40 @@ class EffectDirectionType(Enum):
     BOTH = "both"
 
 
+class RegulatoryEffectSet(models.QuerySet):
+    def with_facet_values(self):
+        return self.prefetch_related("facet_values")
+
+
 class RegulatoryEffect(Searchable, FacetedModel):
-    # Facets:
-    #  * Direction (Discrete, EffectDirectionType)
-    #  * Raw p value (Continuous, float)
-    #  * Signficance (Continuous, float)
-    #  * Effect Size (Continuous, float)
+    class Facet(Enum):
+        DIRECTION = "Direction"  # EffectDirectionType
+        RAW_P_VALUE = "Raw p value"  # float
+        SIGNIFICANCE = "Signficance"  # float
+        EFFECT_SIZE = "Effect Size"  # float
+
+    objects = RegulatoryEffectSet.as_manager()
 
     experiment = models.ForeignKey(Experiment, null=True, on_delete=models.SET_NULL)
     sources = models.ManyToManyField(DNARegion, related_name="regulatory_effects")
     targets = models.ManyToManyField(Feature, related_name="regulatory_effects")
     target_assemblies = models.ManyToManyField(FeatureAssembly, related_name="regulatory_effects")
 
+    @property
+    def direction(self):
+        return self.facet_values.get(facet__name=RegulatoryEffect.Facet.DIRECTION.value).value
+
+    @property
+    def effect_size(self):
+        return self.facet_values.get(facet__name=RegulatoryEffect.Facet.EFFECT_SIZE.value).value
+
+    @property
+    def significance(self):
+        return self.facet_values.get(facet__name=RegulatoryEffect.Facet.SIGNIFICANCE.value).value
+
+    @property
+    def raw_p_value(self):
+        return self.facet_values.get(facet__name=RegulatoryEffect.Facet.RAW_P_VALUE.value).value
+
     def __str__(self):
-        direction = self.facet_values.get(facet__name="Direction")
-        effect_size = self.facet_values.get(facet__name="Effect Size")
-        return f"{direction.value}: {self.sources.count()} source(s) -> {effect_size.num_value} on {self.targets.count()} target(s)"  # noqa: E501
+        return f"{self.direction.value}: {self.sources.count()} source(s) -> {self.effect_size.num_value} on {self.targets.count()} target(s)"  # noqa: E501
--- cegs_portal/search/models/tests/reg_effects_factory.py
+++ cegs_portal/search/models/tests/reg_effects_factory.py
@@ -1,11 +1,8 @@
-import random
-
 import factory
 from factory import post_generation
 from factory.django import DjangoModelFactory
 
 from cegs_portal.search.models import RegulatoryEffect
-from cegs_portal.search.models.reg_effects import EffectDirectionType
 from cegs_portal.search.models.tests.experiment_factory import ExperimentFactory
 
 
@@ -13,18 +10,7 @@ class RegEffectFactory(DjangoModelFactory):
     class Meta:
         model = RegulatoryEffect
 
-    direction = random.choice(
-        [
-            EffectDirectionType.DEPLETED.value,
-            EffectDirectionType.ENRICHED.value,
-            EffectDirectionType.NON_SIGNIFICANT.value,
-            EffectDirectionType.BOTH.value,
-        ]
-    )
     experiment = factory.SubFactory(ExperimentFactory)
-    effect_size = random.random()
-    significance = random.random()
-    raw_p_value = random.random()
 
     @post_generation
     def sources(self, create, extracted, **kwargs):
@@ -58,3 +44,14 @@ class RegEffectFactory(DjangoModelFactory):
             # A list of groups were passed in, use them
             for assembly in extracted:
                 self.target_assemblies.add(assembly)  # pylint: disable=no-member
+
+    @post_generation
+    def facet_values(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for value in extracted:
+                self.facet_values.add(value)  # pylint: disable=no-member
