dab426a1e980914fd15c24fe9b722d4cb24ca836
Thomas Cowart <thomas.cowart@duke.edu>
2022-04-13 11:11:43 -0400

    Speeds up "closest gene" search in cCRE loading script
    Simplifies the "closest gene" queries and uses the FeatureAssembly
    feature_type field to make the queries 6-7x faster.
    
    Also improves the timing log output a bit. Removed "@timer("Save cCREs")"
    decorator on bulk_save because saving to the DB was so fast this info
    wasn't interesting or helpful. Adds timing output for each set of
    LOAD_BATCH_SIZE records loaded.

--- scripts/load_screen_ccres.py
+++ scripts/load_screen_ccres.py
@@ -1,8 +1,7 @@
 import csv
+import time
 
 from django.db import transaction
-from django.db.models import F, IntegerField
-from django.db.models.functions import Abs, Lower, Upper
 from psycopg2.extras import NumericRange
 
 from cegs_portal.search.models import DNARegion, Facet, FacetValue, FeatureAssembly
@@ -14,7 +13,6 @@ CCRE_FACET = Facet.objects.get(name="cCRE Category")
 CCRE_FACET_VALUES = {facet.value: facet for facet in FacetValue.objects.filter(facet_id=CCRE_FACET.id).all()}
 
 
-@timer("Save cCREs")
 def bulk_save(sites):
     with transaction.atomic():
         DNARegion.objects.bulk_create(sites, batch_size=1000)
@@ -32,6 +30,7 @@ def load_ccres(ccres_file, source_file, ref_genome, ref_genome_patch, delimiter=
     new_sites: list[DNARegion] = []
     facets: list[list[FacetValue]] = []
     print("Starting line 0")
+    startTime = time.perf_counter()
     for i, line in enumerate(reader):
         if i % LOAD_BATCH_SIZE == 0 and i != 0:
             print(f"Saving {i - LOAD_BATCH_SIZE}-{i - 1}")
@@ -39,9 +38,14 @@ def load_ccres(ccres_file, source_file, ref_genome, ref_genome_patch, delimiter=
             for site, facet_qset in zip(new_sites, facets):
                 for facet in facet_qset:
                     site.facet_values.add(facet)
+            endTime = time.perf_counter()  # 2
+            runTime = endTime - startTime  # 3
+            print(f"Loaded {LOAD_BATCH_SIZE} cCREs in: {runTime} s")
+            startTime = time.perf_counter()
             new_sites = []
             facets = []
             print(f"Starting line {i}")
+
         chrom_name, dhs_start_str, dhs_end_str, _, accession_id, ccre_categories = line
 
         if "_" in chrom_name:
@@ -54,36 +58,49 @@ def load_ccres(ccres_file, source_file, ref_genome, ref_genome_patch, delimiter=
         dhs_midpoint = (dhs_start + dhs_end) // 2
 
         closest_pos_assembly = (
-            FeatureAssembly.objects.annotate(dist=Abs(Lower(F("location"), output_field=IntegerField()) - dhs_midpoint))
-            .filter(chrom_name=chrom_name, strand="+", ref_genome=ref_genome, feature__feature_type="gene")
-            .order_by("dist")
+            FeatureAssembly.objects.filter(
+                chrom_name=chrom_name,
+                strand="+",
+                ref_genome=ref_genome,
+                feature_type="gene",
+                location__not_gt=NumericRange(dhs_midpoint, dhs_midpoint + 1),
+            )
+            .order_by("-location")
             .first()
         )
 
         closest_neg_assembly = (
-            FeatureAssembly.objects.annotate(dist=Abs(Upper(F("location"), output_field=IntegerField()) - dhs_midpoint))
-            .filter(chrom_name=chrom_name, strand="-", ref_genome=ref_genome, feature__feature_type="gene")
-            .order_by("dist")
+            FeatureAssembly.objects.filter(
+                chrom_name=chrom_name,
+                strand="-",
+                ref_genome=ref_genome,
+                feature_type="gene",
+                location__not_lt=NumericRange(dhs_midpoint - 1, dhs_midpoint),
+            )
+            .order_by("location")
             .first()
         )
 
-        if closest_pos_assembly is None:
-            closest_assembly = closest_neg_assembly
-        elif closest_neg_assembly is None:
-            closest_assembly = closest_pos_assembly
-        elif closest_pos_assembly.dist <= closest_neg_assembly.dist:
-            closest_assembly = closest_pos_assembly
-        else:
-            closest_assembly = closest_neg_assembly
-
-        if closest_assembly is not None:
-            distance = closest_assembly.dist
-            closest_gene = closest_assembly.feature
-            gene_name = closest_assembly.name
-        else:
+        if closest_pos_assembly is None and closest_neg_assembly is None:
+            closest_assembly = None
             distance = -1
             closest_gene = None
             gene_name = "No Gene"
+        elif closest_pos_assembly is None:
+            closest_assembly = closest_neg_assembly
+            distance = closest_neg_assembly.location.lower - dhs_midpoint
+        elif closest_neg_assembly is None or (dhs_midpoint - closest_pos_assembly.location.upper) <= (
+            closest_neg_assembly.location.lower - dhs_midpoint
+        ):
+            closest_assembly = closest_pos_assembly
+            distance = dhs_midpoint - closest_pos_assembly.location.upper
+        else:
+            closest_assembly = closest_neg_assembly
+            distance = closest_neg_assembly.location.lower - dhs_midpoint
+
+        if closest_assembly is not None:
+            closest_gene = closest_assembly.feature
+            gene_name = closest_assembly.name
 
         dhs = DNARegion(
             cell_line=cell_line,
