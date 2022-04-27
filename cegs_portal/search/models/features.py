f92b7ffc24ce0f68768df5a9df403eb6db827160
Thomas Cowart <thomas.cowart@duke.edu>
2022-04-27 11:01:50 -0400

    Add Feature fields to FeatureAssembly
    This is step 1 of 4(?) in combining Feature and Feature assembly.
    
    Step 1: Add Feature Fields to FeatureAssembly
    Step 2: Migrate the Data
    Step 3: Update code to use FeatureAssembly only
    Step 4: Remove Feature from DB, finalize some FeatureAssembly changes
    
    The purpose of this is to simplify the DB schema and gain some performance,
    since there won't be so many joins to get all the necessary data.

--- cegs_portal/search/models/features.py
+++ cegs_portal/search/models/features.py
@@ -14,9 +14,11 @@ class FeatureAssembly(Searchable):
             models.Index(fields=["chrom_name"], name="sfa_chrom_name_index"),
             models.Index(fields=["feature_type"], name="sfa_feature_type_index"),
             GistIndex(fields=["location"], name="sfa_loc_index"),
+            # models.Index(fields=["ensembl_id"], name="sf_ensembl_id_index"), # Add after data is migrated
         ]
 
     chrom_name = models.CharField(max_length=10)
+    ensembl_id = models.CharField(max_length=50, default="No ID")
     ids = models.JSONField(null=True, validators=[validate_gene_ids])
 
     # These values will be returned as 0-index, half-closed. This should be converted to
@@ -26,8 +28,19 @@ class FeatureAssembly(Searchable):
     strand = models.CharField(max_length=1, null=True)
     ref_genome = models.CharField(max_length=20)
     ref_genome_patch = models.CharField(max_length=10)
-    feature = models.ForeignKey("Feature", on_delete=models.CASCADE, related_name="assemblies")
-    feature_type = models.CharField(max_length=50)  # gene, tanscript, etc.
+    feature = models.ForeignKey(
+        "Feature", on_delete=models.CASCADE, related_name="assemblies"
+    )  # remove this after feature data is migrated
+
+    # gene, tanscript, etc.
+    feature_type = models.CharField(max_length=50)
+
+    # gene_type or transcript_type from gencodeannotation.attributes
+    feature_subtype = models.CharField(max_length=50, null=True)
+
+    misc = models.JSONField(null=True)  # exon number, for instance
+
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, null=True, related_name="children")
 
     def __str__(self):
         return f"{self.name} -- {self.chrom_name}:{self.location.lower}-{self.location.upper} ({self.ref_genome})"
