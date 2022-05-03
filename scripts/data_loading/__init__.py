e02a5f5e44426a08fcfc7457329564ab25802df6
Thomas Cowart <thomas.cowart@duke.edu>
2022-05-03 08:56:39 -0400

    Replaces get_closest_gene with faster algorithm
    Previously, get_closest_gene would query the DB for the two closest
    genes on every call.
    
    The new algorithm just does two calls per chromosome (one per strand,
    using the lru_cache decorator to save the result), then uses a binary
    search on the resulting list of genes to find the closest.

--- scripts/data_loading/__init__.py
+++ scripts/data_loading/__init__.py
@@ -1,5 +1,4 @@
-from django.db.models import F, IntegerField
-from django.db.models.functions import Abs, Lower, Upper
+from functools import lru_cache
 
 from cegs_portal.search.models import FeatureAssembly
 
@@ -12,39 +11,101 @@ def check_genome(ref_genome: str, ref_genome_patch: str):
         raise ValueError(f"reference genome patch '{ref_genome_patch}' must be either blank or a series of digits")
 
 
-def get_closest_gene(chrom_name, ref_genome, range_start, range_end):
-    range_midpoint = (range_start + range_end) // 2
-
-    closest_pos_assembly = (
-        FeatureAssembly.objects.annotate(dist=Abs(Lower(F("location"), output_field=IntegerField()) - range_midpoint))
-        .filter(chrom_name=chrom_name, strand="+", ref_genome=ref_genome, feature__feature_type="gene")
-        .order_by("dist")
-        .first()
+@lru_cache(maxsize=1)
+def get_pos_assemblies(chrom_name, ref_genome):
+    return list(
+        FeatureAssembly.objects.filter(
+            chrom_name=chrom_name,
+            strand="+",
+            ref_genome=ref_genome,
+            feature_type="gene",
+        )
+        .order_by("location")
+        .all()
     )
 
-    closest_neg_assembly = (
-        FeatureAssembly.objects.annotate(dist=Abs(Upper(F("location"), output_field=IntegerField()) - range_midpoint))
-        .filter(chrom_name=chrom_name, strand="-", ref_genome=ref_genome, feature__feature_type="gene")
-        .order_by("dist")
-        .first()
+
+@lru_cache(maxsize=1)
+def get_neg_assemblies(chrom_name, ref_genome):
+    return list(
+        FeatureAssembly.objects.filter(
+            chrom_name=chrom_name,
+            strand="-",
+            ref_genome=ref_genome,
+            feature_type="gene",
+        )
+        .order_by("location")
+        .all()
     )
 
-    if closest_pos_assembly is None:
+
+def find_pos_closest(dhs_midpoint, assemblies):
+    start = 0
+    end = len(assemblies)
+    index = (end + start) // 2
+    while True:
+        assembly = assemblies[index]
+        if index == end or index == start:
+            # the loop is hacky, but the binary search only gets _close_ to finding the closest assembly.
+            for i in range(-6, 7):
+                new_assembly = assemblies[min(max(0, index + i), len(assemblies) - 1)]
+                if abs(new_assembly.location.lower - dhs_midpoint) < abs(assembly.location.lower - dhs_midpoint):
+                    assembly = new_assembly
+            return assembly
+
+        if assembly.location.lower >= dhs_midpoint:
+            end = index
+        elif assembly.location.lower < dhs_midpoint:
+            start = index
+
+        index = (end + start) // 2
+
+
+def find_neg_closest(dhs_midpoint, assemblies):
+    start = 0
+    end = len(assemblies)
+    index = (end + start) // 2
+    while True:
+        assembly = assemblies[index]
+        if index == end or index == start:
+            # the loop is hacky, but the binary search only gets _close_ to finding the closest assembly.
+            for i in range(-6, 7):
+                new_assembly = assemblies[min(max(0, index + i), len(assemblies) - 1)]
+                if abs(new_assembly.location.upper - dhs_midpoint) < abs(assembly.location.upper - dhs_midpoint):
+                    assembly = new_assembly
+            return assembly
+
+        if assembly.location.upper >= dhs_midpoint:
+            end = index
+        elif assembly.location.upper < dhs_midpoint:
+            start = index
+
+        index = (end + start) // 2
+
+
+def get_closest_gene(ref_genome, chrom_name, start, end):
+    range_midpoint = (start + end) // 2
+    closest_pos_assembly = find_pos_closest(range_midpoint, get_pos_assemblies(chrom_name, ref_genome))
+
+    closest_neg_assembly = find_neg_closest(range_midpoint, get_neg_assemblies(chrom_name, ref_genome))
+
+    if closest_pos_assembly is None and closest_neg_assembly is None:
+        closest_assembly = None
+        distance = -1
+        gene_name = "No Gene"
+    elif closest_pos_assembly is None:
         closest_assembly = closest_neg_assembly
-    elif closest_neg_assembly is None:
+        distance = abs(range_midpoint - closest_neg_assembly.location.upper)
+    elif closest_neg_assembly is None or abs(range_midpoint - closest_pos_assembly.location.lower) <= abs(
+        closest_neg_assembly.location.upper - range_midpoint
+    ):
         closest_assembly = closest_pos_assembly
-    elif closest_pos_assembly.dist <= closest_neg_assembly.dist:
-        closest_assembly = closest_pos_assembly
+        distance = abs(range_midpoint - closest_pos_assembly.location.lower)
     else:
         closest_assembly = closest_neg_assembly
+        distance = abs(closest_neg_assembly.location.upper - range_midpoint)
 
     if closest_assembly is not None:
-        distance = closest_assembly.dist
-        closest_gene = closest_assembly.feature
         gene_name = closest_assembly.name
-    else:
-        distance = -1
-        closest_gene = None
-        gene_name = "No Gene"
 
-    return closest_gene, closest_assembly, distance, gene_name
+    return closest_assembly, distance, gene_name
