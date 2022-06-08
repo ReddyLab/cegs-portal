496041a22e1583a54c671a1f499aa209cb5fd7b4
Thomas Cowart <thomas.cowart@duke.edu>
2022-06-08 10:01:16 -0400

    Creates experiment_coverage endpoint
    This endpoint will be used to get data for the experiment coverage
    visualization. The client will pass the facet values in a request. The server
    will load and filter the data, then respond with the values for each bucket.
    
    Thus, the heavy lifting will be on the server, where we have more options for
    making filtering fast.

--- cegs_portal/search/views/v1/__init__.py
+++ cegs_portal/search/views/v1/__init__.py
@@ -1,5 +1,6 @@
 from .dna_regions import DNARegion, DNARegionLoc
 from .experiment import ExperimentListView, ExperimentView
+from .experiment_coverage import ExperimentCoverageView
 from .features import Feature, FeatureEnsembl, FeatureLoc
 from .reg_effects import RegEffectView, RegionEffectsView
 from .search import SearchView
