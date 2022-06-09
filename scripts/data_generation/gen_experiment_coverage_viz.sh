ed524a2be016662df2d97fe7cd31409a9bb47665
Thomas Cowart <thomas.cowart@duke.edu>
2022-06-09 16:47:46 -0400

    Generate and read experiment coverage manifest file
    In order to shorten "time to first paint" for the experiment visualization the
    manifest file has been modified to include the data needed to display the visualization
    with no filters selected. Additionally, the references for the different "levels" were
    removed, as the client no longer needs them.
    
    The experiment page has been update to read the new manifest format and display the
    buckets from it. Some modifications have been made in anticipation of adding the server
    calls to get the filtered data.
    
    A new shell script has been added that will generate all necessary data files for the
    visualization.

---
+++ scripts/data_generation/gen_experiment_coverage_viz.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -euo pipefail
+
+EXPERIMENT=$1
+GENOME=$2
+OUTPUT_DIR=$3
+
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_manifest; gen_experiment_coverage_manifest.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\")"
+python manage.py shell -c "from os.path import join; from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(join(\"$OUTPUT_DIR\", \"level1.json\"), \"$EXPERIMENT\", \"$GENOME\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"1\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"2\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"3\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"4\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"5\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"6\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"7\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"8\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"9\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"10\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"11\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"12\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"13\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"14\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"15\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"16\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"17\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"18\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"19\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"20\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"21\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"22\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"X\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"Y\")"
+python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_viz; gen_experiment_coverage_viz.run(\"$OUTPUT_DIR\", \"$EXPERIMENT\", \"$GENOME\", 100_000, \"MT\")"
