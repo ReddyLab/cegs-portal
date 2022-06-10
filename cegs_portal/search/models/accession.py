098abc7b74ae738117b89e88450d23a4a42cb9c2
Thomas Cowart <thomas.cowart@duke.edu>
2022-06-10 11:56:29 -0400

    experiment_coverage endpoint can be POSTed and returns relevant data
    The endpoint reads one of the relevant files for the experiment data
    and just returns the keys. This is very basic, but from here we
    can add the data processing and improve the data loading (currently, 211 mb
    takes 3s to load, we'll need to do better if this is going to be "on demand"
    filtering).

--- cegs_portal/search/models/accession.py
+++ cegs_portal/search/models/accession.py
@@ -1,15 +1,6 @@
-from re import fullmatch
-
-from django.core.exceptions import ValidationError
 from django.db import models
 
-
-def validate_accession_id(value):
-    if value is not None and fullmatch("DCP[A-Z][0-9A-Z]{8}", value) is None:
-        raise ValidationError(
-            f"""{value} is not of the form "DCPYXXXXXXXX", where Y is a capital
-            letter indicating the type of object, and X is a number or capital letter"""
-        )
+from cegs_portal.search.models.validators import validate_accession_id
 
 
 class Accessioned(models.Model):
--- cegs_portal/search/views/v1/experiment_coverage.py
+++ cegs_portal/search/views/v1/experiment_coverage.py
@@ -1,4 +1,10 @@
+import json
+from os.path import join
+
+from django.contrib.staticfiles import finders
+
 from cegs_portal.search.json_templates.v1.experiment_coverage import experiment_coverage
+from cegs_portal.search.models.validators import validate_accession_id
 from cegs_portal.search.views.custom_views import TemplateJsonView
 from cegs_portal.search.views.view_utils import JSON_MIME
 from cegs_portal.utils.http_exceptions import Http500
@@ -22,10 +28,19 @@ class ExperimentCoverageView(TemplateJsonView):
                 * in
         """
         options = super().request_options(request)
-        options["re_page"] = int(request.GET.get("re_page", 1))
+        try:
+            body = json.loads(request.body)
+        except Exception as e:
+            raise Http500(f"Invalid request body:\n{request.body}\n\nError:\n{e}")
+
+        try:
+            options["filters"] = body["filters"]
+        except Exception as e:
+            raise Http500(f'Invalid request body, no "filters" object:\n{request.body}\n\nError:\n{e}')
+
         return options
 
-    def get(self, request, options, data):
+    def post(self, request, options, data, exp_id):
         raise Http500(
             (
                 f'This is a JSON-only API. Please request using "Accept: {JSON_MIME}" header or '
@@ -33,8 +48,10 @@ class ExperimentCoverageView(TemplateJsonView):
             )
         )
 
-    def get_json(self, request, options, data):
-        return
+    def post_data(self, options, exp_id):
+        validate_accession_id(exp_id)
+        level1_filename = finders.find(join("search", "experiments", exp_id, "level1.json"))
+        with open(level1_filename, "r") as level1_file:
+            level1 = json.load(level1_file)
 
-    def get_data(self, options):
-        return
+        return level1
