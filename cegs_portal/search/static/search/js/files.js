749975526d844b330c1adfbd99ac3996ab0f8f4c
Thomas Cowart <thomas.cowart@duke.edu>
2022-06-15 15:08:15 -0400

    Client request new display data
    Added the code so the client will request new display data when the filters
    are changed.
    
    When the new data is returned it is then rendered.

--- cegs_portal/search/static/search/js/files.js
+++ cegs_portal/search/static/search/js/files.js
@@ -8,3 +8,23 @@ export function getJson(path) {
             return response.json();
         });
 }
+
+export function postJson(path, body) {
+    let init = {
+        method: "POST",
+        body: body,
+        headers: {
+            "Accept": "application/json"
+        },
+        mode: "same-origin"
+    };
+
+    return fetch(path, init)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`${path} fetch failed: ${response.status} ${response.statusText}`);
+            }
+
+            return response.json();
+        });
+}
