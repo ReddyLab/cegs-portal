ba6dd0624abe39ce3ab02f807cfa8f8baef14a7b
Thomas Cowart <thomas.cowart@duke.edu>
2022-06-21 16:29:04 -0400

    Combine filter and "display transform" step
    Previously, they were different steps because the ostensible pipeline was
    discrete filter -> continuous filter -> display transform. A recent change
    combined discrete and continuous filter steps. This gave me the idea that
    display transform could also be combined. In addition to being faster, this
    should also take less memory because, instead of lists of objects being created
    and appended to, its just some numbers being incremented.

--- cegs_portal/search/views/v1/experiment_coverage.py
+++ cegs_portal/search/views/v1/experiment_coverage.py
@@ -1,6 +1,6 @@
 import json
 import pickle
-from functools import lru_cache
+from functools import lru_cache, reduce
 from os.path import join
 
 from django.contrib.staticfiles import finders
@@ -76,9 +76,10 @@ def filter_data(filters, data):
         if len(sf_with_selections) > 0:
             for interval in chromosome["source_intervals"]:
                 sources = interval["sources"]
-                new_sources = []
+                new_sources = 0
+                new_target_buckets = set()
                 for effects, target_buckets in sources:
-                    new_regeffects = []
+                    new_regeffects = 0
                     for disc_facets, effect_size, sig in effects:
                         if skip_disc_facets or (
                             len([sf for sf in selected_sf if not sf.isdisjoint(disc_facets)]) == len_selected_sf
@@ -92,24 +93,39 @@ def filter_data(filters, data):
                                 and sig >= sig_interval[0]
                                 and sig <= sig_interval[1]
                             ):
-                                new_regeffects.append((disc_facets, effect_size, sig))
+                                new_regeffects += 1
+                                new_target_buckets.update(target_buckets)
 
-                    if len(new_regeffects) > 0:
-                        new_sources.append((new_regeffects, target_buckets))
+                    if new_regeffects > 0:
+                        new_sources += 1
 
-                if len(new_sources) > 0:
+                if new_sources > 0:
                     new_data["chromosomes"][c]["source_intervals"].append(
-                        {"start": interval["start"], "sources": new_sources}
+                        {
+                            "start": interval["start"],
+                            "count": new_sources,
+                            "assoc_targets": flatten(list(new_target_buckets)),
+                        }
                     )
         else:
-            new_data["chromosomes"][c]["source_intervals"] = chromosome["source_intervals"]
+            new_data["chromosomes"][c]["source_intervals"] = [
+                {
+                    "start": interval["start"],
+                    "count": len(interval["sources"]),
+                    "assoc_targets": flatten(
+                        list(reduce(lambda x, source: x | set(source[1]), interval["sources"], set()))
+                    ),
+                }
+                for interval in chromosome["source_intervals"]
+            ]
 
         if len(tf_with_selections) > 0:
             for interval in chromosome["target_intervals"]:
                 targets = interval["targets"]
-                new_targets = []
+                new_targets = 0
+                new_source_buckets = set()
                 for effects, source_buckets in targets:
-                    new_regeffects = []
+                    new_regeffects = 0
                     for disc_facets, effect_size, sig in effects:
                         if skip_disc_facets or (
                             len([tf for tf in selected_tf if not tf.isdisjoint(disc_facets)]) == len_selected_tf
@@ -123,17 +139,31 @@ def filter_data(filters, data):
                                 and sig >= sig_interval[0]
                                 and sig <= sig_interval[1]
                             ):
-                                new_regeffects.append((disc_facets, effect_size, sig))
+                                new_regeffects += 1
+                                new_source_buckets.update(source_buckets)
 
-                    if len(new_regeffects) > 0:
-                        new_targets.append((new_regeffects, source_buckets))
+                    if new_regeffects > 0:
+                        new_targets += 1
 
-                if len(new_targets) > 0:
+                if new_targets > 0:
                     new_data["chromosomes"][c]["target_intervals"].append(
-                        {"start": interval["start"], "targets": new_targets}
+                        {
+                            "start": interval["start"],
+                            "count": new_targets,
+                            "assoc_sources": flatten(list(new_source_buckets)),
+                        }
                     )
         else:
-            new_data["chromosomes"][c]["target_intervals"] = chromosome["target_intervals"]
+            new_data["chromosomes"][c]["target_intervals"] = [
+                {
+                    "start": interval["start"],
+                    "count": len(interval["targets"]),
+                    "assoc_sources": flatten(
+                        list(reduce(lambda x, target: x | set(target[1]), interval["targets"], set()))
+                    ),
+                }
+                for interval in chromosome["target_intervals"]
+            ]
 
     min_effect = effect_size_interval[0] if min_effect == INFINITY else min_effect
     max_effect = effect_size_interval[1] if max_effect == NEG_INFINITY else max_effect
@@ -141,42 +171,12 @@ def filter_data(filters, data):
     min_sig = sig_interval[0] if min_sig == INFINITY else min_sig
     max_sig = sig_interval[1] if max_sig == NEG_INFINITY else max_sig
 
-    return new_data, (min_effect, max_effect), (min_sig, max_sig)
+    new_data["continuous_intervals"] = {
+        "effect": (min_effect, max_effect),
+        "sig": (min_sig, max_sig),
+    }
 
-
-def display_transform(data):
-    result = {"chromosomes": []}
-    for chromosome in data["chromosomes"]:
-        chrom_data = {
-            "chrom": chromosome["chrom"],
-            "bucket_size": chromosome["bucket_size"],
-            "source_intervals": [],
-            "target_intervals": [],
-        }
-
-        for interval in chromosome["source_intervals"]:
-            new_interval = {"start": interval["start"], "count": len(interval["sources"])}
-
-            targets = set()
-            for source in interval["sources"]:
-                targets.update(source[1])
-
-            new_interval["assoc_targets"] = flatten(list(targets))
-            chrom_data["source_intervals"].append(new_interval)
-
-        for interval in chromosome["target_intervals"]:
-            new_interval = {"start": interval["start"], "count": len(interval["targets"])}
-
-            sources = set()
-            for target in interval["targets"]:
-                sources.update(target[1])
-
-            new_interval["assoc_sources"] = flatten(list(sources))
-            chrom_data["target_intervals"].append(new_interval)
-
-        result["chromosomes"].append(chrom_data)
-
-    return result
+    return new_data
 
 
 @lru_cache(maxsize=100)
@@ -228,11 +228,4 @@ class ExperimentCoverageView(TemplateJsonView):
     def post_data(self, options, exp_acc_id):
         validate_accession_id(exp_acc_id)
         level1 = load_coverage(exp_acc_id)
-        filters = options["filters"]
-        new_data, effect_interval, sig_interval = filter_data(filters, level1)
-        display_data = display_transform(new_data)
-        display_data["continuous_intervals"] = {
-            "effect": effect_interval,
-            "sig": sig_interval,
-        }
-        return display_data
+        return filter_data(options["filters"], level1)
