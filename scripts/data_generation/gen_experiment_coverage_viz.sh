b6b6ebb4e3fdfc603075eaf4d4f575268fe5ad57
Thomas Cowart <thomas.cowart@duke.edu>
2022-07-12 16:07:35 -0400

    Add some more automation to gen_experiment_coverage_viz
    It's now fully self contained, no need to pass any arguments in.

--- scripts/data_generation/gen_experiment_coverage_viz.sh
+++ scripts/data_generation/gen_experiment_coverage_viz.sh
@@ -1,38 +1,43 @@
 #!/bin/sh
 set -euo pipefail
 
-EXPERIMENT=$1
-GENOME=$2
-OUTPUT_DIR=$3
+gen_data() {
+    local EXPERIMENT=$1
+    local GENOME=$2
+    local OUTPUT_DIR=$3
 
-# Install cov_viz from github: https://github.com/ReddyLab/cov_viz
-# cov_viz requires rust, which can be downloaded/installed from https://www.rust-lang.org
+    # Install cov_viz from github: https://github.com/ReddyLab/cov_viz
+    # cov_viz requires rust, which can be downloaded/installed from https://www.rust-lang.org
 
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME}
-# I think gen_experiment_coverage_manifest needs to be rewritten in rust as well
-# python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_manifest; gen_experiment_coverage_manifest.run(\"$OUTPUT_DIR\", \"$GENOME\")"
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr1
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr2
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr3
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr4
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr5
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr6
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr7
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr8
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr9
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr10
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr11
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr12
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr13
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr14
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr15
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr16
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr17
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr18
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr19
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr20
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr21
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr22
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrX
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrY
-cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrMT
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME}
+    # I think gen_experiment_coverage_manifest needs to be rewritten in rust as well
+    # python manage.py shell -c "from scripts.data_generation import gen_experiment_coverage_manifest; gen_experiment_coverage_manifest.run(\"$OUTPUT_DIR\", \"$GENOME\")"
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr1
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr2
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr3
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr4
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr5
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr6
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr7
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr8
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr9
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr10
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr11
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr12
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr13
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr14
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr15
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr16
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr17
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr18
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr19
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr20
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr21
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr22
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrX
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrY
+    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrMT
+}
+
+gen_data DCPE00000002 GRCH37 ./cegs_portal/static_data/search/experiments/DCPE00000002
+gen_data DCPE00000004 GRCH38 ./cegs_portal/static_data/search/experiments/DCPE00000004
