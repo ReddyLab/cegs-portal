49ac5e7821434c6e1f3919c65a392c68b46b2d3c
Thomas Cowart <thomas.cowart@duke.edu>
2022-09-01 15:20:20 -0400

    Intersect bed file with experiment viewer
    If a user "uploads" (it's not actually uploaded) a bed file in an
    experiment view, the buckets that contain bed file regions will
    be highlighted. Or more accurately, buckets that DONT contain bed file
    regions will be faded.

---
+++ cegs_portal/search/static/search/js/bed.js
@@ -0,0 +1,21 @@
+import { getLines } from "./text.js";
+
+const getRegions = function(text) {
+    let regions = {}
+    for (let line of getLines(text)) {
+        let fields = line.split('\t');
+        let chrom = fields[0].replace("chr", "");
+        let start = parseInt(fields[1]);
+        let end = parseInt(fields[2]);
+
+        if (regions[chrom] == undefined) {
+            regions[chrom] = [];
+        }
+
+        regions[chrom].push([start, end]);
+    }
+
+    return regions;
+}
+
+export { getRegions }
