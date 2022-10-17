a9879b52e2fcd3a40256b897866ac02303ee12d7
Thomas Cowart <thomas.cowart@duke.edu>
2022-10-17 16:24:29 -0400

    Replace Cell Line and Tissue Type with Biosample
    If a Cell Line is like a "class" a biosample is like an "object" of
    that class.
    
    Each biosample can have significant TBD metadata associated with that
    differentiates it from other biosamples of the same cell line.
    
    Some biosamples may also not belong to a well known/named cell line.
    For instance, they may be patient samples.

--- cegs_portal/search/models/experiment.py
+++ cegs_portal/search/models/experiment.py
@@ -7,18 +7,32 @@ from cegs_portal.search.models.facets import Faceted
 from cegs_portal.search.models.file import File
 
 
-class CellLine(models.Model):
-    line_name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.line_name
-
-
 class TissueType(models.Model):
-    tissue_type = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=4096, null=True)
 
     def __str__(self):
-        return self.tissue_type
+        return self.name
+
+
+class CellLine(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=4096, null=True)
+    tissue_type = models.ForeignKey(TissueType, on_delete=models.PROTECT)
+    tissue_type_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Biosample(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    cell_line = models.ForeignKey(CellLine, on_delete=models.PROTECT)
+    cell_line_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=4096, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.cell_line_name})"
 
 
 class Experiment(Accessioned, Faceted):
@@ -29,6 +43,7 @@ class Experiment(Accessioned, Faceted):
     description = models.CharField(max_length=4096, null=True)
     experiment_type = models.CharField(max_length=100, null=True)
     name = models.CharField(max_length=512)
+    biosamples = models.ManyToManyField(Biosample, related_name="experiment_data")
     other_files = models.ManyToManyField(File, related_name="experiments")
 
     def assay(self):
@@ -39,15 +54,12 @@ class Experiment(Accessioned, Faceted):
 
 
 class ExperimentDataFile(models.Model):
-    cell_line = models.CharField(max_length=100)
-    cell_lines = models.ManyToManyField(CellLine, related_name="experiment_data")
     description = models.CharField(max_length=4096, null=True)
     experiment = models.ForeignKey("Experiment", on_delete=models.CASCADE, related_name="data_files")
     filename = models.CharField(max_length=512)
     ref_genome = models.CharField(max_length=20)
     ref_genome_patch = models.CharField(max_length=10)
     significance_measure = models.CharField(max_length=2048)
-    tissue_types = models.ManyToManyField(TissueType, related_name="experiment_data")
 
     def __str__(self):
         return f"{self.filename}:\n{self.description}"
--- utils/experiment.py
+++ utils/experiment.py
@@ -6,8 +6,11 @@ from typing import IO, Any
 from cegs_portal.search.models import (
     AccessionIdLog,
     AccessionType,
+    Biosample,
+    CellLine,
     Experiment,
     ExperimentDataFile,
+    TissueType,
 )
 
 from .file import FileMetadata
@@ -16,23 +19,20 @@ from .misc import get_delimiter
 
 class ExperimentDatafileMetadata:
     description: str
-    cell_line: str
     filename: str
     ref_genome: str
     ref_genome_patch: str
     significance_measure: str
 
     def __init__(self, file_metadata: dict[str, str]):
-        self.cell_line = file_metadata["cell_line"]
-        self.description = file_metadata["description"]
+        self.description = file_metadata.get("description", None)
         self.filename = file_metadata["file"]
         self.ref_genome = file_metadata["ref_genome"]
-        self.ref_genome_patch = file_metadata["ref_genome_patch"]
+        self.ref_genome_patch = file_metadata.get("ref_genome_patch", None)
         self.significance_measure = file_metadata["significance_measure"]
 
     def db_save(self, experiment: Experiment):
         data_file = ExperimentDataFile(
-            cell_line=self.cell_line,
             description=self.description,
             experiment=experiment,
             filename=self.filename,
@@ -41,7 +41,28 @@ class ExperimentDatafileMetadata:
             significance_measure=self.significance_measure,
         )
         data_file.save()
+
         return data_file
+
+
+class ExperimentBiosample:
+    cell_line: str
+    tissue_type: str
+    description: str
+
+    def __init__(self, bio_metadata: dict[str, str]):
+        self.name = bio_metadata.get("name", None)
+        self.description = bio_metadata.get("description", None)
+        self.cell_line = bio_metadata["cell_line"]
+        self.tissue_type = bio_metadata["tissue_type"]
+
+    def db_save(self):
+        tt = TissueType.objects.get_or_create(name=self.tissue_type)
+        cl = CellLine.objects.get_or_create(name=self.cell_line, tissue_type=tt, tissue_type_name=self.tissue_type)
+        biosample = Biosample.objects.create(
+            name=self.name, description=self.description, cell_line=cl, cell_line_name=self.cell_line
+        )
+        biosample.save()
 
 
 class ExperimentMetadata:
@@ -51,6 +72,7 @@ class ExperimentMetadata:
     name: str
     filename: str
     accession_id: str
+    biosamples: list[ExperimentBiosample]
     other_file_metadata: list[FileMetadata]
 
     def __init__(self, experiment_dict: dict[str, Any], experiment_filename: str):
@@ -61,6 +83,7 @@ class ExperimentMetadata:
         self.filename = experiment_filename
         self.data_file_metadata = []
         self.other_file_metadata = []
+        self.biosamples = [ExperimentBiosample(sample) for sample in experiment_dict["biosamples"]]
         for data in experiment_dict["data"]:
             self.data_file_metadata.append(ExperimentDatafileMetadata(data))
         for file in experiment_dict.get("other_files", []):
@@ -86,6 +109,8 @@ class ExperimentMetadata:
             message=self.description[:200],
         )
         accession_log.save()
+        for biosample in self.biosamples:
+            biosample.db_save()
         return experiment
 
     def db_del(self):
