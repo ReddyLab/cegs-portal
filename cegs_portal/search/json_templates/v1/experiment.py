5feec867483d17ec37fd016b6cae08e78a670c14
Thomas Cowart <thomas.cowart@duke.edu>
2022-10-27 15:20:52 -0400

    Update tests and templates for Biosamples
    Model changes means tests and templates need to be updated! Biosamples
    are incorporated into everything now, and all tests pass.

--- cegs_portal/search/json_templates/v1/experiment.py
+++ cegs_portal/search/json_templates/v1/experiment.py
@@ -9,18 +9,10 @@ def experiments(experiments_obj: Iterable[Experiment]):
             "id": e.id,
             "name": e.name,
             "description": e.description,
-            "cell_lines": None,
-            "tissue_types": None,
+            "biosamples": [biosample(b) for b in e.biosamples.all()],
         }
         for e in experiments_obj
     ]
-
-    for e, result in zip(experiments_obj, results):
-        if hasattr(e, "cell_lines"):
-            result["cell_lines"] = [line for line in e.cell_lines]  # type: ignore[attr-defined]
-
-        if hasattr(e, "tissue_types"):
-            result["tissue_types"] = [tt for tt in e.tissue_types]  # type: ignore[attr-defined]
 
     return results
 
@@ -31,23 +23,25 @@ def experiment(experiment_obj: Experiment, options: Optional[dict[str, Any]] = N
         "name": experiment_obj.name,
         "description": experiment_obj.description,
         "assay": experiment_obj.experiment_type,
-        "cell_lines": None,
+        "biosamples": [biosample(b) for b in experiment_obj.biosamples.all()],
         "data_files": [data_file(f) for f in experiment_obj.data_files.all()],
         "other_files": [other_file(f) for f in experiment_obj.other_files.all()],
     }
 
-    if hasattr(experiment_obj, "cell_lines"):
-        result["cell_lines"] = experiment_obj.cell_lines  # type: ignore[attr-defined]
-
     return result
+
+
+def biosample(biosample_obj: ExperimentDataFile):
+    return {
+        "name": biosample_obj.name,
+        "cell_line": biosample_obj.cell_line_name,
+    }
 
 
 def data_file(data_file_obj: ExperimentDataFile):
     return {
         "filename": data_file_obj.filename,
         "description": data_file_obj.description,
-        "cell_lines": [str(line) for line in data_file_obj.cell_lines.all()],
-        "tissue_types": [str(tt) for tt in data_file_obj.tissue_types.all()],
         "assembly": f"{data_file_obj.ref_genome}.{data_file_obj.ref_genome_patch or '0'}",
     }
 
--- cegs_portal/search/views/v1/experiment.py
+++ cegs_portal/search/views/v1/experiment.py
@@ -17,13 +17,15 @@ class ExperimentView(TemplateJsonView):
         if experi is None:
             raise Http404(f"No experiment with id {exp_id} found.")
 
-        experi_cell_lines = set()
-        experi_tissue_types = set()
         experi_assemblies = set()
         for f in experi.data_files.all():
-            experi_cell_lines.update(f.cell_lines.all())
-            experi_tissue_types.update(f.tissue_types.all())
             experi_assemblies.add(f"{f.ref_genome}.{f.ref_genome_patch or '0'}")
+
+        experi_cell_lines = set()
+        experi_tissue_types = set()
+        for bios in experi.biosamples.all():
+            experi_cell_lines.add(bios.cell_line_name)
+            experi_tissue_types.add(bios.cell_line.tissue_type_name)
 
         setattr(experi, "cell_lines", experi_cell_lines)
         setattr(experi, "tissue_types", experi_tissue_types)
