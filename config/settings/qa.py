d24bc222352268d08c599c13203451ca589c3daf
Thomas Cowart <thomas.cowart@duke.edu>
2022-12-22 14:24:37 -0500

    Adds an allowed host for intra-pod requests
    In particular, for prometheus to query the /metrics endpoint

--- config/settings/qa.py
+++ config/settings/qa.py
@@ -7,7 +7,7 @@ DEBUG = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[".duke.edu", ".cluster.local", "localhost"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[".duke.edu", "", ".cegsptl", ".cluster.local", "localhost"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
