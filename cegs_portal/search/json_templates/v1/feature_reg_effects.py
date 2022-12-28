600ae720a2be1f893e17b88cd9a94d10e5a7aa44
Thomas Cowart <thomas.cowart@duke.edu>
2022-12-28 11:48:05 -0500

    Fix mypy typing error in feature reg effect json templates

--- cegs_portal/search/json_templates/v1/feature_reg_effects.py
+++ cegs_portal/search/json_templates/v1/feature_reg_effects.py
@@ -1,9 +1,9 @@
-from typing import Any, Optional, TypedDict
+from typing import Any, NotRequired, Optional, TypedDict
 
 from cegs_portal.search.models import RegulatoryEffectObservation
 from cegs_portal.utils.pagination_types import Pageable, PageableJson
 
-ExperimentJson = TypedDict("ExperimentJson", {"id": int, "name": str})
+ExperimentJson = TypedDict("ExperimentJson", {"accession_id": str, "name": str})
 RegulatoryEffectObservationJson = TypedDict(
     "RegulatoryEffectObservationJson",
     {
@@ -12,7 +12,7 @@ RegulatoryEffectObservationJson = TypedDict(
         "effect_size": Optional[float],
         "significance": Optional[float],
         "raw_p_value": Optional[float],
-        "experiment": ExperimentJson,
+        "experiment": NotRequired[ExperimentJson],
         "source_ids": list[str],
         "target_ids": list[str],
     },
@@ -22,16 +22,18 @@ RegulatoryEffectObservationJson = TypedDict(
 def regulatory_effect(
     reg_effect: RegulatoryEffectObservation, options: Optional[dict[str, Any]] = None
 ) -> RegulatoryEffectObservationJson:
-    result = {
+    result: RegulatoryEffectObservationJson = {
         "accession_id": reg_effect.accession_id,
         "direction": reg_effect.direction,
         "effect_size": reg_effect.effect_size,
         "significance": reg_effect.significance,
         "raw_p_value": reg_effect.raw_p_value,
-        "experiment": {"accession_id": reg_effect.experiment.accession_id, "name": reg_effect.experiment.name},
         "source_ids": [source.accession_id for source in reg_effect.sources.all()],
         "target_ids": [target.accession_id for target in reg_effect.targets.all()],
     }
+
+    if reg_effect.experiment is not None:
+        result["experiment"] = {"accession_id": reg_effect.experiment.accession_id, "name": reg_effect.experiment.name}
 
     return result
 
@@ -39,7 +41,7 @@ def regulatory_effect(
 def feature_reg_effects(
     reg_effects: Pageable[RegulatoryEffectObservation], options: Optional[dict[str, Any]] = None
 ) -> PageableJson:
-    results = {
+    results: PageableJson = {
         "object_list": [regulatory_effect(re, options) for re in reg_effects.object_list],
         "page": reg_effects.number,
         "has_next_page": reg_effects.has_next(),
