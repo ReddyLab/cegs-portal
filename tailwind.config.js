33e0278c5f9e1bc5c379af89d8c034cfceec2c97
Thomas Cowart <thomas.cowart@duke.edu>
2023-02-24 11:10:56 -0500

    Gets tabs on experiment page working
    tailwind elements was installed via npm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-  mode: 'jit',
-  content: [
-    './cegs_portal/**/*.html',
-    './cegs_portal/**/*.js',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: "jit",
+    content: ["./cegs_portal/**/*.html", "./cegs_portal/**/*.js", "./node_modules/tw-elements/dist/js/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("tw-elements/dist/plugin")],
+};
