8cebac5ba4337d876ab103b85fe370e143389d7b
Thomas Cowart <thomas.cowart@duke.edu>
2023-04-04 11:13:15 -0400

    Improve comments on "unused fixtures"

--- cegs_portal/get_expr_data/conftest.py
+++ cegs_portal/get_expr_data/conftest.py
@@ -6,7 +6,9 @@ from psycopg2.extras import NumericRange
 
 from cegs_portal.get_expr_data.models import ReoSourcesTargets
 from cegs_portal.get_expr_data.view_models import EXPR_DATA_DIR
-from cegs_portal.search.conftest import login_client  # noqa:
+from cegs_portal.search.conftest import (  # noqa: F401 used as fixture in tests.py
+    login_client,
+)
 from cegs_portal.search.models import RegulatoryEffectObservation
 from cegs_portal.search.models.tests.dna_feature_factory import DNAFeatureFactory
 from cegs_portal.search.models.tests.experiment_factory import ExperimentFactory
