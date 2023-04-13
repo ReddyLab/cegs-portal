02af7f36310c69712ab3e4866e995c0c15180331
Thomas Cowart <thomas.cowart@duke.edu>
2023-04-13 14:02:19 -0400

    Update exprs 4-6 to split exper and analysis loading

--- scripts/data_loading/DCPEXPR00000004_load_bounds_scCERES_mhc_2021_analysis.py
+++ scripts/data_loading/DCPEXPR00000004_load_bounds_scCERES_mhc_2021_analysis.py
@@ -6,16 +6,14 @@ from psycopg2.extras import NumericRange
 from cegs_portal.search.models import (
     AccessionIds,
     AccessionType,
+    Analysis,
     DNAFeature,
     DNAFeatureType,
-    Experiment,
     Facet,
     FacetValue,
     RegulatoryEffectObservation,
 )
-from utils import ExperimentMetadata, timer
-
-from . import get_closest_gene
+from utils import AnalysisMetadata, timer
 
 DIR_FACET = Facet.objects.get(name="Direction")
 DIR_FACET_VALUES = {facet.value: facet for facet in FacetValue.objects.filter(facet_id=DIR_FACET.id).all()}
@@ -37,11 +35,8 @@ GRNA_FACET_VALUES = {facet.value: facet for facet in FacetValue.objects.filter(f
 # In postgres the objects automatically get their id's when bulk_created but
 # objects that reference the bulk_created objects (i.e., with foreign keys) don't
 # get their foreign keys updated. The for loops do that necessary updating.
-def bulk_save(grnas, effects, effect_directions, sources, source_facets, targets):
+def bulk_save(effects, effect_directions, sources, source_facets, targets):
     with transaction.atomic():
-        print("Adding gRNA Regions")
-        DNAFeature.objects.bulk_create(grnas, batch_size=1000)
-
         print("Adding RegulatoryEffectObservations")
         RegulatoryEffectObservation.objects.bulk_create(effects, batch_size=1000)
 
@@ -66,18 +61,17 @@ def bulk_save(grnas, effects, effect_directions, sources, source_facets, targets
 
 # loading does buffered writes to the DB, with a buffer size of 10,000 annotations
 @timer("Load Reg Effects")
-def load_reg_effects(
-    ceres_file, accession_ids, experiment, region_source, cell_line, ref_genome, ref_genome_patch, delimiter=","
-):
+def load_reg_effects(ceres_file, accession_ids, analysis, ref_genome, ref_genome_patch, delimiter=","):
+    experiment = analysis.experiment
+    cell_line = experiment.biosamples.first().cell_line_name
     reader = csv.DictReader(ceres_file, delimiter=delimiter, quoting=csv.QUOTE_NONE)
-    sites = []
-    site_facets = []
+    sources = []
+    source_facets = []
     effects = []
     effect_directions = []
     target_assembiles = []
     grnas = {}
     existing_grna_facets = {}
-    grnas_to_save = []
     for i, line in enumerate(reader):
         # every other line in this file is basically a duplicate of the previous line
         if i % 2 == 0:
@@ -85,7 +79,7 @@ def load_reg_effects(
         grna_id = line["grna"]
 
         if grna_id in grnas:
-            region = grnas[grna_id]
+            guide = grnas[grna_id]
         else:
             existing_grna_facets[grna_id] = set()
 
@@ -95,48 +89,30 @@ def load_reg_effects(
                 continue
 
             if len(grna_info) == 5:
-                chrom_name, grna_start_str, grna_end_str, strand, _grna_seq = grna_info
+                chrom_name, grna_start_str, _grna_end_str, _strand, _grna_seq = grna_info
             elif len(grna_info) == 6:
-                chrom_name, grna_start_str, grna_end_str, _x, _y, _grna_seq = grna_info
-                strand = "-"
+                chrom_name, grna_start_str, _grna_end_str, _x, _y, _grna_seq = grna_info
 
             grna_start = int(grna_start_str)
-            grna_end = int(grna_end_str)
             grna_location = NumericRange(grna_start, grna_start + 20, "[]")
 
-            closest_gene, distance, gene_name = get_closest_gene(ref_genome, chrom_name, grna_start, grna_end)
-
             try:
-                region = DNAFeature.objects.get(
+                guide = DNAFeature.objects.get(
+                    misc__grna=grna_id,
                     cell_line=cell_line,
-                    chrom_name=chrom_name,
                     location=grna_location,
                     ref_genome=ref_genome,
                     ref_genome_patch=ref_genome_patch,
                     feature_type=DNAFeatureType.GRNA,
                 )
-                existing_grna_facets[grna_id].update(region.facet_values.all())
-            except DNAFeature.DoesNotExist:
-                region = DNAFeature(
-                    accession_id=accession_ids.incr(AccessionType.GRNA),
-                    experiment_accession_id=experiment.accession_id,
-                    cell_line=cell_line,
-                    chrom_name=chrom_name,
-                    closest_gene=closest_gene,
-                    closest_gene_distance=distance,
-                    closest_gene_name=gene_name,
-                    closest_gene_ensembl_id=closest_gene.ensembl_id,
-                    location=grna_location,
-                    misc={"grna": grna_id},
-                    ref_genome=ref_genome,
-                    ref_genome_patch=ref_genome_patch,
-                    feature_type=DNAFeatureType.GRNA,
-                    source=region_source,
-                    strand=strand,
+            except DNAFeature.MultipleObjectsReturned as e:
+                print(
+                    f"{cell_line} {chrom_name}:{grna_location} {ref_genome} { ref_genome_patch} {DNAFeatureType.GRNA}"
                 )
-                grnas_to_save.append(region)
-            grnas[grna_id] = region
-        sites.append(region)
+                raise e
+            existing_grna_facets[grna_id].update(guide.facet_values.all())
+            grnas[grna_id] = guide
+        sources.append(guide)
 
         grna_type = line["type"]
         grna_facets = set()
@@ -157,7 +133,7 @@ def load_reg_effects(
         elif grna_type == "positive_control_other":
             grna_facets.add(GRNA_FACET_VALUES["Positive Control"])
             grna_facets.add(GRNA_FACET_VALUES["Positive Control (other)"])
-        site_facets.append(grna_facets - existing_grna_facets[grna_id])
+        source_facets.append(grna_facets - existing_grna_facets[grna_id])
         existing_grna_facets[grna_id].update(grna_facets)
 
         significance = float(line["pval_fdr_corrected"])
@@ -180,7 +156,8 @@ def load_reg_effects(
         effect = RegulatoryEffectObservation(
             accession_id=accession_ids.incr(AccessionType.REGULATORY_EFFECT_OBS),
             experiment=experiment,
-            experiment_accession_id=experiment.accession_id,
+            experiment_accession=experiment,
+            analysis=analysis,
             facet_num_values={
                 RegulatoryEffectObservation.Facet.EFFECT_SIZE.value: effect_size,
                 RegulatoryEffectObservation.Facet.RAW_P_VALUE.value: float(line["p_val"]),
@@ -190,50 +167,41 @@ def load_reg_effects(
         target_assembiles.append(target_assembly)
         effects.append(effect)
         effect_directions.append(direction)
-    bulk_save(grnas_to_save, effects, effect_directions, sites, site_facets, target_assembiles)
+    bulk_save(effects, effect_directions, sources, source_facets, target_assembiles)
 
 
-def unload_reg_effects(experiment_metadata):
-    try:
-        print(experiment_metadata.accession_id)
-        experiment = Experiment.objects.get(accession_id=experiment_metadata.accession_id)
-    except Experiment.DoesNotExist:
-        return
-    except Exception as e:
-        raise e
-
-    RegulatoryEffectObservation.objects.filter(experiment=experiment).delete()
-    for file in experiment.other_files.all():
-        DNAFeature.objects.filter(source=file).delete()
-    experiment_metadata.db_del()
+def unload_reg_effects(analysis_metadata):
+    analysis = Analysis.objects.get(
+        experiment_id=analysis_metadata.experiment_accession_id, name=analysis_metadata.name
+    )
+    RegulatoryEffectObservation.objects.filter(analysis=analysis).delete()
+    analysis_metadata.db_del(analysis)
 
 
-def check_filename(experiment_filename: str):
-    if len(experiment_filename) == 0:
-        raise ValueError(f"scCERES experiment filename '{experiment_filename}' must not be blank")
+def check_filename(analysis_filename: str):
+    if len(analysis_filename) == 0:
+        raise ValueError(f"scCERES experiment filename '{analysis_filename}' must not be blank")
 
 
-def run(experiment_filename):
-    with open(experiment_filename) as experiment_file:
-        experiment_metadata = ExperimentMetadata.json_load(experiment_file)
-    check_filename(experiment_metadata.name)
+def run(analysis_filename):
+    with open(analysis_filename) as analysis_file:
+        analysis_metadata = AnalysisMetadata.json_load(analysis_file)
+    check_filename(analysis_metadata.name)
 
     # Only run unload_reg_effects if you want to delete the experiment, all
     # associated reg effects, and any DNAFeatures created from the DB.
     # Please note that it won't reset DB id numbers, so running this script with
     # unload_reg_effects() uncommented is not, strictly, idempotent.
-    # unload_reg_effects(experiment_metadata)
+    # unload_reg_effects(analysis_metadata)
 
-    experiment = experiment_metadata.db_save()
+    analysis = analysis_metadata.db_save()
 
-    with AccessionIds(message=f"{experiment.accession_id}: {experiment.name}"[:200]) as accession_ids:
-        for ceres_file, file_info, delimiter in experiment_metadata.metadata():
+    with AccessionIds(message=f"{analysis.accession_id}: {analysis.name}"[:200]) as accession_ids:
+        for ceres_file, file_info, delimiter in analysis_metadata.metadata():
             load_reg_effects(
                 ceres_file,
                 accession_ids,
-                experiment,
-                experiment.other_files.all()[0],
-                file_info.cell_line,
+                analysis,
                 file_info.ref_genome,
                 file_info.ref_genome_patch,
                 delimiter,
