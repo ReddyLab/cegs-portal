b47a5edcde3c2e8dd1ff7c702e993a63c05cc1f9
Thomas Cowart <thomas.cowart@duke.edu>
2023-04-27 16:38:51 -0400

    Convert function to async

--- cegs_portal/search/static/search/js/downloadStatusWorker.js
+++ cegs_portal/search/static/search/js/downloadStatusWorker.js
@@ -1,18 +1,18 @@
 onmessage = function (statusURLData) {
-    let statusURL = statusURLData.data;
-    let getStatus = (url, tries) => {
-        return fetch(url)
-            .then((response) => response.json())
-            .then((json) => {
-                let progress = json["file progress"];
-                postMessage(progress);
-                if (progress == "in_preparation") {
-                    setTimeout(getStatus(url, tries - 1), 500);
-                }
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+    let getStatus = async (url, tries) => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            let progress = json["file progress"];
+            postMessage(progress);
+            if (progress == "in_preparation") {
+                setTimeout(getStatus(url, tries - 1), 500);
+            }
+        } catch (err) {
+            console.log(err);
+        }
     };
-    Promise.all([getStatus(statusURL, 5)]);
+
+    let statusURL = statusURLData.data;
+    getStatus(statusURL, 5);
 };
