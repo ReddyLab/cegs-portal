d8b7c019b736c2927727b764b449b3fd829af7d7
Thomas Cowart <thomas.cowart@duke.edu>
2023-06-21 12:33:11 -0400

    Fix linter failure in dna_feature test

--- cegs_portal/search/json_templates/v1/tests/test_dna_features.py
+++ cegs_portal/search/json_templates/v1/tests/test_dna_features.py
@@ -39,7 +39,6 @@ def test_feature(feature: DNAFeature):
         "ref_genome_patch": feature.ref_genome_patch,
         "parent": feature.parent if feature.parent is not None else None,
         "parent_accession_id": feature.parent_accession_id if feature.parent is not None else None,
-        "parent": feature.parent.name if feature.parent else None,
         "misc": feature.misc,
     }
     if feature.closest_gene is not None:
