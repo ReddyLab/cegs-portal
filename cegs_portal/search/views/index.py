7c3554f30532f54275e0b944358fd29d0e79dec3
Thomas Cowart <thomas.cowart@duke.edu>
2023-06-30 11:44:05 -0400

    Remove test task call
    Test was successful!

--- cegs_portal/search/views/index.py
+++ cegs_portal/search/views/index.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 
 from cegs_portal.search.forms import SearchForm
-from cegs_portal.search.tasks import count_beans
 
 
 def index(request):
-    rsp = count_beans(12)
-    print(rsp)
     form = SearchForm()
     return render(request, "search/index.html", {"form": form})
