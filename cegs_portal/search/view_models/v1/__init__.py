327162196fe79cb638190059c88c9920f7458a4b
Thomas Cowart <thomas.cowart@duke.edu>
2023-07-26 15:59:47 -0400

    Use analysis accession for expr cov data
    An experiment may have multiple analyses associated with it. Each
    analysis should have its own coverage map because the numbers involved
    can change.
    
    These changes change the code to support per-analysis coverage maps.

--- cegs_portal/search/view_models/v1/__init__.py
+++ cegs_portal/search/view_models/v1/__init__.py
@@ -1,4 +1,5 @@
 from .dna_features import DNAFeatureSearch, IdType, LocSearchType
 from .experiment import ExperimentSearch
+from .experiment_coverage import ExperimentCoverageSearch
 from .reg_effects import RegEffectSearch
 from .search import Search
--- scripts/data_generation/gen_experiment_coverage_viz.sh
+++ scripts/data_generation/gen_experiment_coverage_viz.sh
@@ -10,12 +10,12 @@ gen_dir() {
 }
 
 gen_data() {
-    local EXPERIMENT=$1
+    local ANALYSIS=$1
     local GENOME=$2
     local OUTPUT_DIR=$3
     local DEFAULT_FACETS=$4
 
-    echo $EXPERIMENT
+    echo $ANALYSIS
     gen_dir "${OUTPUT_DIR}"
 
     # Install cov_viz from github: https://github.com/ReddyLab/cov_viz
@@ -30,48 +30,48 @@ gen_data() {
         cp "./scripts/data_generation/data/grch38.json" $OUTPUT_DIR
     fi
 
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME}
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME}
     cov_viz_manifest ${GENOME} ${OUTPUT_DIR}/level1.bin ${OUTPUT_DIR} ${DEFAULT_FACETS}
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr1
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr2
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr3
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr4
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr5
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr6
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr7
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr8
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr9
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr10
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr11
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr12
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr13
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr14
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr15
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr16
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr17
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr18
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr19
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr20
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr21
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chr22
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrX
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrY
-    cov_viz ${OUTPUT_DIR} ${EXPERIMENT} ${GENOME} 100000 chrMT
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr1
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr2
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr3
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr4
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr5
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr6
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr7
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr8
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr9
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr10
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr11
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr12
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr13
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr14
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr15
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr16
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr17
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr18
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr19
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr20
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr21
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chr22
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chrX
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chrY
+    cov_viz ${OUTPUT_DIR} ${ANALYSIS} ${GENOME} 100000 chrMT
 }
 
 default_facets=`python manage.py shell -c "from cegs_portal.search.models import FacetValue; print(' '.join(str(facet.id) for facet in FacetValue.objects.filter(value__in=['Depleted Only', 'Enriched Only', 'Mixed']).all()))"`
 
-gen_data DCPEXPR00000001 GRCH37 ./cegs_portal/static_data/search/experiments/DCPEXPR00000001 "${default_facets}"
-gen_data DCPEXPR00000002 GRCH37 ./cegs_portal/static_data/search/experiments/DCPEXPR00000002 "${default_facets}"
+gen_data DCPAN00000000 GRCH37 ./cegs_portal/static_data/search/experiments/DCPEXPR00000001/DCPAN00000000 "${default_facets}"
+gen_data DCPAN00000002 GRCH37 ./cegs_portal/static_data/search/experiments/DCPEXPR00000002/DCPAN00000002 "${default_facets}"
 
 # gen_data DCPEXPR00000003 needs some changes to the cov_viz and related programs to work. Some
 # of the  REOs have null effect sizes because the "direction" of the effect is "both". Deferring
 # for now. It's also not clear how to surface that in the facet filter.
-# gen_data DCPEXPR00000003 GRCH37 ./cegs_portal/static_data/search/experiments/DCPEXPR00000003 "${default_facets}"
+# gen_data DCPAN00000001 GRCH37 ./cegs_portal/static_data/search/experiments/DCPEXPR00000003/DCPAN00000001 "${default_facets}"
 
-gen_data DCPEXPR00000004 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000004 "${default_facets}"
-gen_data DCPEXPR00000005 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000005 "${default_facets}"
-gen_data DCPEXPR00000006 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000006 "${default_facets}"
-gen_data DCPEXPR00000007 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000007 "${default_facets}"
-gen_data DCPEXPR00000008 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000008 "${default_facets}"
-gen_data DCPEXPR00000009 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000009 "${default_facets}"
+gen_data DCPAN00000003 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000004/DCPAN00000003 "${default_facets}"
+gen_data DCPAN00000004 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000005/DCPAN00000004 "${default_facets}"
+gen_data DCPAN00000005 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000006/DCPAN00000005 "${default_facets}"
+gen_data DCPAN00000006 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000007/DCPAN00000006 "${default_facets}"
+gen_data DCPAN00000007 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000008/DCPAN00000007 "${default_facets}"
+gen_data DCPAN00000008 GRCH38 ./cegs_portal/static_data/search/experiments/DCPEXPR00000009/DCPAN00000008 "${default_facets}"
