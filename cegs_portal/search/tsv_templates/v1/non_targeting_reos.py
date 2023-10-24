f884ea1eeefdb6ec2af22b41b47434cd32ed7869
DHE\bmd32 <brian.davis722@duke.edu>
2023-10-24 13:03:58 -0400

    Adding bed6 format to tsv download
    Added the first required fields and added feature name as name

--- cegs_portal/search/tsv_templates/v1/non_targeting_reos.py
+++ cegs_portal/search/tsv_templates/v1/non_targeting_reos.py
@@ -1,11 +1,39 @@
 def non_targeting_regulatory_effects(data):
     reo_page, feature = data
     tsv_data = []
-    tsv_data.append(['Chromosome Location', 'Effect Size', 'Direction', 'Significance', 'Distance', 'Feature Type', 'Experiment'])
+    tsv_data.append(
+        [
+            "chrom",
+            "chromStart",
+            "chromEnd",
+            "name",
+            "strand",
+            "score",
+            "Effect Size",
+            "Direction",
+            "Significance",
+            "Distance",
+            "Feature Type",
+            "Experiment",
+        ]
+    )
 
     for reo in reo_page:
         first_source = reo.sources.all()[0]
-        row = [first_source.chrom_name, first_source.location.lower, first_source.location.upper, reo.effect_size, reo.direction, reo.significance, first_source.closest_gene_distance, first_source.get_feature_type_display(), reo.experiment.name]
+        row = [
+            first_source.chrom_name,
+            first_source.location.lower,
+            first_source.location.upper,
+            feature.name,
+            feature.strand,
+            " ",
+            reo.effect_size,
+            reo.direction,
+            reo.significance,
+            first_source.closest_gene_distance,
+            first_source.get_feature_type_display(),
+            reo.experiment.name,
+        ]
 
         tsv_data.append(row)
 
