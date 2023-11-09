cf84e90f08a9886af36a1fa5fc4610ecddee2a5a
DHE\bmd32 <brian.davis722@duke.edu>
2023-11-09 08:57:32 -0500

    Added case if there are no targets

--- cegs_portal/search/tsv_templates/v1/reg_effects.py
+++ cegs_portal/search/tsv_templates/v1/reg_effects.py
@@ -3,21 +3,25 @@ from cegs_portal.search.templatetags.custom_helpers import format_float, if_stra
 
 
 def tested_element(source, target):
-    return (
-        f"{source.chrom_name}:{source.location.lower}-{source.location.upper}:{if_strand(source.strand)}:{target.name}"
-    )
+    return f"{source.chrom_name}:{source.location.lower}-{source.location.upper}:{if_strand(source.strand)}:{target.name if target else 'N/A'}"
 
 
 def bed6_output(reos):
     tsv_data = []
     for reo in reos:
-        for target in reo.targets.all():
-            for source in reo.sources.all():
+        sources = reo.sources.all()
+        targets = reo.targets.all()
+
+        if not targets:
+            targets = [None]
+
+        for target in targets:
+            for source in sources:
                 row = [
                     source.chrom_name,
                     source.location.lower,
                     source.location.upper,
-                    tested_element(source, target),
+                    tested_element(source, target) if target else "N/A",
                     "0",
                     if_strand(source.strand),
                 ]
@@ -48,20 +52,26 @@ def reg_effects(reos, options):
     )
 
     for reo in reos:
-        for target in reo.targets.all():
-            for source in reo.sources.all():
+        sources = reo.sources.all()
+        targets = reo.targets.all()
+
+        if not targets:
+            targets = [None]
+
+        for target in targets:
+            for source in sources:
                 row = [
                     source.chrom_name,
                     source.location.lower,
                     source.location.upper,
-                    tested_element(source, target),
+                    tested_element(source, target) if target else "N/A",
                     "0",
                     if_strand(source.strand),
                     reo.effect_size,
                     reo.direction,
                     format_float(reo.significance),
                     reo.experiment.name,
-                    target.name,
+                    target.name if target else "N/A",
                 ]
 
                 tsv_data.append(row)
@@ -90,13 +100,19 @@ def target_reg_effects(reos, options):
     )
 
     for reo in reos:
-        for source in reo.sources.all():
-            for target in reo.targets.all():
+        sources = reo.sources.all()
+        targets = reo.targets.all()
+
+        if not targets:
+            targets = [None]
+
+        for source in sources:
+            for target in targets:
                 row = [
                     source.chrom_name,
                     source.location.lower,
                     source.location.upper,
-                    tested_element(source, target),
+                    tested_element(source, target) if target else "N/A",
                     "0",
                     if_strand(source.strand),
                     reo.effect_size,
