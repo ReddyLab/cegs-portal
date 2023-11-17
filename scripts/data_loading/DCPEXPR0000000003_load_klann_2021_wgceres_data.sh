35e261e35a1367b54746271110a416654e0b65d0
Thomas Cowart <thomas.cowart@duke.edu>
2023-11-17 10:54:21 -0500

    Remove closest ccre file for EXPR3

--- scripts/data_loading/DCPEXPR0000000003_load_klann_2021_wgceres_data.sh
+++ scripts/data_loading/DCPEXPR0000000003_load_klann_2021_wgceres_data.sh
@@ -18,3 +18,5 @@ echo "Loading DCPEXPR0000000003"
 
 python manage.py shell -c "from scripts.data_loading import DCPEXPR0000000003_load_klann_2021_wgceres_experiment; DCPEXPR0000000003_load_klann_2021_wgceres_experiment.run(\"${EXPERIMENT_FILE}\", \"${CLOSEST_CCRE_FILE}\")"
 python manage.py shell -c "from scripts.data_loading import DCPEXPR0000000003_load_klann_2021_wgceres_analysis; DCPEXPR0000000003_load_klann_2021_wgceres_analysis.run(\"${ANALYSIS_FILE}\")"
+
+rm ${CLOSEST_CCRE_FILE}
