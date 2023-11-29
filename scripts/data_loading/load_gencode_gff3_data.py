d94733fcb0283d3c8e3960cc889030791ba143ae
Thomas Cowart <thomas.cowart@duke.edu>
2023-11-29 10:14:37 -0500

    Make range type explicit
    "[)" (half-open) is the default, but I think its useful to be explicit.

--- scripts/data_loading/load_gencode_gff3_data.py
+++ scripts/data_loading/load_gencode_gff3_data.py
@@ -72,7 +72,7 @@ def load_genome_annotations(genome_annotations, ref_genome, ref_genome_patch, ve
             # Adjust start/end to use 0-based indexing
             start = int(start) - 1
             end = int(end)
-            region = GencodeRegion(chrom_name=chrom_name, base_range=NumericRange(start, end))
+            region = GencodeRegion(chrom_name=chrom_name, base_range=NumericRange(start, end, "[)"))
             region.save()
             continue
 
