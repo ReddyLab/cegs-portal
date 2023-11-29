7db975d674ae86eb427da544b7f5d1199c3c3e9b
Thomas Cowart <thomas.cowart@duke.edu>
2023-11-29 10:13:54 -0500

    Add usage guardrails to FeatureIds
    Only one instance of FeatureIds should be in use at a time. Two at a time
    will generate duplicate ids.

--- utils/features.py
+++ utils/features.py
@@ -4,9 +4,18 @@ from cegs_portal.search.models import DNAFeature
 
 
 class FeatureIds:
-    current_feature_id = None
+    # Only one "FeatureIds" should be in use at a time. Otherwise
+    # the FeatureIds instances will potentially assign the same ids
+    # to different rows, and one of the commits will fail.
+    in_use = False
+
+    def __init__(self):
+        self.current_feature_id = None
 
     def __enter__(self):
+        assert FeatureIds.in_use is False
+        FeatureIds.in_use = True
+
         last_object = DNAFeature.objects.all().values_list("id", flat=True).order_by("-id")
         if len(last_object) > 0:
             self.current_feature_id = last_object[0]
@@ -25,5 +34,6 @@ class FeatureIds:
         return self.current_feature_id
 
     def __exit__(self, exc_type, exc_value, exc_tb):
+        FeatureIds.in_use = False
         with connection.cursor() as cursor:
             cursor.execute("ALTER SEQUENCE search_dnafeature_id_seq RESTART WITH %s", (self.current_feature_id,))
