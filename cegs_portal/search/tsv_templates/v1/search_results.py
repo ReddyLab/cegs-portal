19cef9646a2b794f3cabee466ad87d61a826cc63
Thomas Cowart <thomas.cowart@duke.edu>
2024-01-23 11:16:30 -0500

    Add tests, use target names
    Adds tests, because we need tests
    item "names" should include the target name, if it exists

--- cegs_portal/search/tsv_templates/v1/search_results.py
+++ cegs_portal/search/tsv_templates/v1/search_results.py
@@ -2,26 +2,27 @@ from cegs_portal.search.helpers.options import is_bed6
 from cegs_portal.search.templatetags.custom_helpers import if_strand
 
 
-def item_name(source):
-    return (
-        f"{source.chrom_name}:{source.location.lower}-{source.location.upper}:{if_strand(source.strand)}:{source.name}"
-    )
+def item_name(source, target):
+    return f"{source.chrom_name}:{source.location.lower}-{source.location.upper}:{if_strand(source.strand)}:{f'{target.name}' if target is not None else ''}"
 
 
 def sig_reos_bed6(reos):
     tsv_data = []
     for reo in reos:
-        for source in reo.sources.all():
-            row = [
-                source.chrom_name,
-                source.location.lower,
-                source.location.upper,
-                item_name(source),
-                "0",
-                if_strand(source.strand),
-            ]
+        targets = list(reo.targets.all())
+        targets = targets if len(targets) > 0 else [None]
+        for target in targets:
+            for source in reo.sources.all():
+                row = [
+                    source.chrom_name,
+                    source.location.lower,
+                    source.location.upper,
+                    item_name(source, target),
+                    "0",
+                    if_strand(source.strand),
+                ]
 
-            tsv_data.append(row)
+                tsv_data.append(row)
 
     return tsv_data
 
@@ -50,23 +51,26 @@ def sig_reos(data, options):
     )
 
     for reo in reos:
-        for source in reo.sources.all():
-            row = [
-                source.chrom_name,
-                source.location.lower,
-                source.location.upper,
-                item_name(source),
-                "0",
-                if_strand(source.strand),
-                reo.effect_size,
-                reo.direction,
-                reo.significance,
-                source.closest_gene_distance,
-                source.get_feature_type_display(),
-                reo.experiment.name,
-                reo.accession_id,
-            ]
+        targets = list(reo.targets.all())
+        targets = targets if len(targets) > 0 else [None]
+        for target in targets:
+            for source in reo.sources.all():
+                row = [
+                    source.chrom_name,
+                    source.location.lower,
+                    source.location.upper,
+                    item_name(source, target),
+                    "0",
+                    if_strand(source.strand),
+                    reo.effect_size,
+                    reo.direction,
+                    reo.significance,
+                    source.closest_gene_distance,
+                    source.get_feature_type_display(),
+                    reo.experiment.name,
+                    reo.accession_id,
+                ]
 
-            tsv_data.append(row)
+                tsv_data.append(row)
 
     return tsv_data
