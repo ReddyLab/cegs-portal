07d8f27d306df28ec815a6156a84fe6cead4256a
Thomas Cowart <thomas.cowart@duke.edu>
2024-03-01 11:32:40 -0500

    Add new test
    This test checks that features abutting existing cCREs don't "overlap"

--- scripts/data_loading/tests/test_experiment_saving.py
+++ scripts/data_loading/tests/test_experiment_saving.py
@@ -105,3 +105,53 @@ def test_ccre_associations(experiment_metadata):
     clean_features()
 
     assert len(generated_features()) == 0
+
+
+@pytest.mark.usefixtures("ccres", "gene")
+def test_close_ccre_associations(experiment_metadata):
+    # Make sure the algorithm works correctly for features that abut existing cCREs
+    # Neither of these features should "overlap" so they should both result in
+    # two new pseudo cCREs
+
+    # This has to happen hear because load_experiment does DB calls on load
+    # which isn't allowed in the pytest suite outside of tests with a django_db mark
+    from scripts.data_loading.load_experiment import Experiment, FeatureRow
+
+    def load_features(_):
+        features = [
+            FeatureRow(
+                name="0",
+                chrom_name="chr1",
+                location=(90, 100, "[)"),
+                strand="+",
+                genome_assembly="GRCh38",
+                cell_line="iPSC",
+                feature_type="gRNA",
+                facets=["Targeting"],
+                misc={"grna": "0"},
+            ),
+            FeatureRow(
+                name="1",
+                chrom_name="chr1",
+                location=(200, 220, "[)"),
+                strand="+",
+                genome_assembly="GRCh38",
+                cell_line="iPSC",
+                feature_type="gRNA",
+                facets=["Targeting"],
+                misc={"grna": "1"},
+            ),
+        ]
+        return features, None
+
+    old_ids = ccre_ids()
+
+    Experiment(experiment_metadata).load(load_features).save()
+
+    for feature in generated_features():
+        for ccre in [ccre for ccre in feature.associated_ccres.all()]:
+            assert ccre.id not in old_ids
+
+    clean_features()
+
+    assert len(generated_features()) == 0
