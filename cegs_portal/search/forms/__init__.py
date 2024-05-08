7a692e329af5cda720ff7166e059bb0aaf6dc4e2
DHE\bmd32 <brian.davis722@duke.edu>
2024-05-08 15:43:49 -0400

    Updating home page for user experience

--- cegs_portal/search/forms/__init__.py
+++ cegs_portal/search/forms/__init__.py
@@ -2,4 +2,12 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label="Search")
+    query = forms.CharField(label="Search", initial="HLA-A")
+
+
+class SearchGeneForm(forms.Form):
+    query = forms.CharField(label="Search", initial="HLA-A")
+
+
+class SearchLocationForm(forms.Form):
+    query = forms.CharField(label="Search", initial="chr8:79263597-79294735")
