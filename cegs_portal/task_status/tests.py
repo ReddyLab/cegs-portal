18f836cd04d508ecdfad00a1f63deff2152c6bec
Thomas Cowart <thomas.cowart@duke.edu>
2024-05-15 16:24:00 -0400

    Adds task status list view tests

--- cegs_portal/task_status/tests.py
+++ cegs_portal/task_status/tests.py
@@ -1,6 +1,10 @@
+import json
+
 import pytest
 
+from cegs_portal.conftest import SearchClient
 from cegs_portal.task_status.decorators import handle_error
+from cegs_portal.task_status.json_templates.task_status import task_statuses
 from cegs_portal.task_status.models import TaskStatus
 
 pytestmark = pytest.mark.django_db
@@ -35,3 +39,56 @@ def test_task_status_handle_error(task: TaskStatus):
 
     assert task.status == TaskStatus.TaskState.ERROR
     assert task.error_message == "Bad Value"
+
+
+def test_task_status_json(task: TaskStatus):
+    assert task_statuses([task]) == [
+        {
+            "id": task.id,
+            "status": task.status,
+            "description": task.description,
+            "error_message": None,
+            "created": task.created.isoformat(),
+            "modified": task.modified.isoformat(),
+        }
+    ]
+
+
+def test_task_list_view(task: TaskStatus, login_client: SearchClient):
+    task.user = login_client.user
+    task.save()
+
+    response = login_client.get(f"/task_status/{login_client.username}?accept=application/json")
+    assert response.status_code == 200
+
+    json_content = json.loads(response.content)
+    assert json_content == {
+        "object_list": [
+            {
+                "id": str(task.id),
+                "status": task.status,
+                "description": task.description,
+                "error_message": None,
+                "created": task.created.isoformat(),
+                "modified": task.modified.isoformat(),
+            }
+        ],
+        "page": 1,
+        "has_next_page": False,
+        "has_prev_page": False,
+        "num_pages": 1,
+    }
+
+
+def test_no_task_list_view(task: TaskStatus, login_client: SearchClient):
+    response = login_client.get(f"/task_status/{login_client.username}?accept=application/json")
+    assert response.status_code == 200
+
+    json_content = json.loads(response.content)
+    assert json_content == {
+        "object_list": [],
+        "page": 1,
+        "has_next_page": False,
+        "has_prev_page": False,
+        "num_pages": 1,
+    }
