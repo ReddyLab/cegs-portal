b7bff1299363416b8dd71e27fcbb23e272c71170
Thomas Cowart <thomas.cowart@duke.edu>
2024-05-16 14:11:50 -0400

    Adds support for getting experiment loading status
    This adds support to the upload view that enables users
    to get upload task status. The "upload finished" html
    includes a link to the task status and the the json
    returns the task uuid which can be used to query the status.

--- cegs_portal/uploads/views/uploads.py
+++ cegs_portal/uploads/views/uploads.py
@@ -2,12 +2,12 @@ from functools import partial
 
 from django.contrib.auth.decorators import permission_required
 from django.db import transaction
-from django.http import HttpResponseRedirect
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.urls import reverse
 from huey.contrib.djhuey import db_task
 
 from cegs_portal.get_expr_data.models import ReoSourcesTargets, ReoSourcesTargetsSigOnly
+from cegs_portal.search.views.view_utils import JSON_MIME
 from cegs_portal.task_status.decorators import handle_error
 from cegs_portal.task_status.models import TaskStatus
 from cegs_portal.uploads.data_generation import gen_all_coverage
@@ -25,6 +25,8 @@ def upload(request):
         form = UploadFileForm(request.POST, request.FILES)
 
         if form.is_valid():
+            print("VALID FORM")
+            json_response = request.headers.get("accept") == JSON_MIME or request.POST.get("accept") == JSON_MIME
             experiment_accession = request.POST["experiment_accession"]
             experiment_data = None
             analysis_data = None
@@ -39,42 +41,55 @@ def upload(request):
             elif (analysis_file := request.FILES.get("analysis_file")) is not None:
                 analysis_data = analysis_file
 
-            handle_upload(experiment_data, analysis_data, experiment_accession, request.user)
-            return HttpResponseRedirect(reverse("uploads:upload_complete"))
+            match experiment_file, analysis_file:
+                case (None, None):
+                    desc = "Null Upload"
+                case (_, None):
+                    desc = "Experiment Upload"
+                case (None, _):
+                    desc = "Analysis Upload"
+                case (_, _):
+                    desc = "Experiment and Analysis Upload"
+
+            task_status = TaskStatus(user=request.user, description=f"({experiment_accession}) {desc}")
+            handle_upload(experiment_data, analysis_data, experiment_accession, task_status, request.user)
+            if json_response:
+                return JsonResponse({"task_status_id": task_status.id})
+            else:
+                return render(request, "uploads/upload_complete.html", {"task_status": task_status})
     else:
         form = UploadFileForm()
     return render(request, "uploads/upload.html", {"upload_form": form})
 
 
-def upload_complete(request):
-    return render(request, "uploads/upload_complete.html", {})
-
-
 @db_task()
-def handle_upload(experiment_file, analysis_file, experiment_accession, user):
-    match experiment_file, analysis_file:
-        case (None, None):
-            desc = "Null Upload"
-        case (_, None):
-            desc = "Experiment Upload"
-        case (None, _):
-            desc = "Analysis Upload"
-        case (_, _):
-            desc = "Experiment and Analysis Upload"
-
-    status = TaskStatus(user=user, description=f"({experiment_accession}) {desc}")
-    status.start()
+def handle_upload(experiment_file, analysis_file, experiment_accession, task_status, user):
+    task_status.start()
 
     if experiment_file is not None:
-        expr_load(experiment_file, experiment_accession)
+        try:
+            expr_load(experiment_file, experiment_accession)
+        except Exception as e:
+            task_status.error(str(e))
+            raise
+
         transaction.on_commit(
-            handle_error(partial(add_experiment_to_user, experiment_accession=experiment_accession, user=user), status)
+            handle_error(
+                partial(add_experiment_to_user, experiment_accession=experiment_accession, user=user), task_status
+            )
         )
 
     if analysis_file is not None:
-        analysis_accession = an_load(analysis_file, experiment_accession)
+        try:
+            analysis_accession = an_load(analysis_file, experiment_accession)
+        except Exception as e:
+            task_status.error(str(e))
+            raise
+
         ReoSourcesTargets.load_analysis(analysis_accession)
         ReoSourcesTargetsSigOnly.load_analysis(analysis_accession)
-        transaction.on_commit(handle_error(partial(gen_all_coverage, analysis_accession=analysis_accession), status))
+        transaction.on_commit(
+            handle_error(partial(gen_all_coverage, analysis_accession=analysis_accession), task_status)
+        )
 
-    transaction.on_commit(lambda: status.finish())
+    transaction.on_commit(lambda: task_status.finish())
