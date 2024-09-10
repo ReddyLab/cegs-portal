8d13610125f10d89b1c5d10ebc95abca2a260a2a
Thomas Cowart <thomas.cowart@duke.edu>
2024-09-10 12:04:59 -0400

    Removes data_files.list(), combine contexts
    Don't need to write out the contents of data_files to the console.
    
    by combining contexts we don't have to indent quite so much.

--- cegs_portal/uploads/data_loading/compressed.py
+++ cegs_portal/uploads/data_loading/compressed.py
@@ -8,20 +8,17 @@ from .experiment_metadata import AnalysisMetadata, ExperimentMetadata
 
 
 def load(compressed_file, experiment_accession_id):
-    with tarfile.open(fileobj=compressed_file, mode="r:gz") as data_files:
-        data_files.list()
+    with tarfile.open(fileobj=compressed_file, mode="r:gz") as data_files, tempfile.TemporaryDirectory() as dir_name:
+        data_files.extractall(dir_name)
 
-        with tempfile.TemporaryDirectory() as dir_name:
-            data_files.extractall(dir_name)
+        experiment_filename = os.path.join(dir_name, "experiment.json")
+        expr_data_filename = os.path.join(dir_name, "tested_elements.tsv")
+        analysis_filename = os.path.join(dir_name, "analysis001.json")
+        analysis_data_filename = os.path.join(dir_name, "observations.tsv")
 
-            experiment_filename = os.path.join(dir_name, "experiment.json")
-            expr_data_filename = os.path.join(dir_name, "tested_elements.tsv")
-            analysis_filename = os.path.join(dir_name, "analysis001.json")
-            analysis_data_filename = os.path.join(dir_name, "observations.tsv")
+        metadata = ExperimentMetadata.load(experiment_filename, experiment_accession_id)
+        Experiment(metadata).load(expr_data_filename).save()
 
-            metadata = ExperimentMetadata.load(experiment_filename, experiment_accession_id)
-            Experiment(metadata).load(expr_data_filename).save()
-
-            metadata = AnalysisMetadata.load(analysis_filename, experiment_accession_id)
-            analysis = Analysis(metadata).load(analysis_data_filename).save()
-            return analysis.accession_id
+        metadata = AnalysisMetadata.load(analysis_filename, experiment_accession_id)
+        analysis = Analysis(metadata).load(analysis_data_filename).save()
+        return analysis.accession_id
