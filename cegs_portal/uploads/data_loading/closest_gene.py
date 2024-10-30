00448e2e4a80ff4270f9979516970d3434f62228
Thomas Cowart <thomas.cowart@duke.edu>
2024-10-30 12:51:07 -0400

    Make closest_gene_distance signed when uploading data
    Also fix some type errors and improve variable names.
    We're dealing with genes in a lot of places, not generic "features"

--- cegs_portal/uploads/data_loading/closest_gene.py
+++ cegs_portal/uploads/data_loading/closest_gene.py
@@ -1,10 +1,11 @@
+import sys
 from functools import lru_cache
 
 from cegs_portal.search.models import DNAFeature, DNAFeatureType
 
 
 @lru_cache(maxsize=1)
-def get_pos_features(chrom_name, ref_genome):
+def get_pos_genes(chrom_name, ref_genome):
     return list(
         DNAFeature.objects.filter(
             chrom_name=chrom_name,
@@ -19,7 +20,7 @@ def get_pos_features(chrom_name, ref_genome):
 
 
 @lru_cache(maxsize=1)
-def get_neg_features(chrom_name, ref_genome):
+def get_neg_genes(chrom_name, ref_genome):
     return list(
         DNAFeature.objects.filter(
             chrom_name=chrom_name,
@@ -33,51 +34,51 @@ def get_neg_features(chrom_name, ref_genome):
     )
 
 
-def find_pos_closest(dhs_midpoint, features):
-    if len(features) == 0:
+def find_pos_closest(midpoint, genes):
+    if len(genes) == 0:
         return None
 
     start = 0
-    end = len(features)
+    end = len(genes)
     index = (end + start) // 2
     while True:
-        feature = features[index]
+        gene = genes[index]
         if index == end or index == start:
             # the loop is hacky, but the binary search only gets _close_ to finding the closest feature.
             for i in range(-6, 7):
-                new_feature = features[min(max(0, index + i), len(features) - 1)]
-                if abs(new_feature["location"].lower - dhs_midpoint) < abs(feature["location"].lower - dhs_midpoint):
-                    feature = new_feature
-            return feature
+                new_gene = genes[min(max(0, index + i), len(genes) - 1)]
+                if abs(new_gene["location"].lower - midpoint) < abs(gene["location"].lower - midpoint):
+                    gene = new_gene
+            return gene
 
-        if feature["location"].lower >= dhs_midpoint:
+        if gene["location"].lower >= midpoint:
             end = index
-        elif feature["location"].lower < dhs_midpoint:
+        elif gene["location"].lower < midpoint:
             start = index
 
         index = (end + start) // 2
 
 
-def find_neg_closest(dhs_midpoint, features):
-    if len(features) == 0:
+def find_neg_closest(midpoint, genes):
+    if len(genes) == 0:
         return None
 
     start = 0
-    end = len(features)
+    end = len(genes)
     index = (end + start) // 2
     while True:
-        feature = features[index]
+        gene = genes[index]
         if index == end or index == start:
             # the loop is hacky, but the binary search only gets _close_ to finding the closest feature.
             for i in range(-6, 7):
-                new_feature = features[min(max(0, index + i), len(features) - 1)]
-                if abs(new_feature["location"].upper - dhs_midpoint) < abs(feature["location"].upper - dhs_midpoint):
-                    feature = new_feature
-            return feature
+                new_gene = genes[min(max(0, index + i), len(genes) - 1)]
+                if abs(new_gene["location"].upper - midpoint) < abs(gene["location"].upper - midpoint):
+                    gene = new_gene
+            return gene
 
-        if feature["location"].upper >= dhs_midpoint:
+        if gene["location"].upper >= midpoint:
             end = index
-        elif feature["location"].upper < dhs_midpoint:
+        elif gene["location"].upper < midpoint:
             start = index
 
         index = (end + start) // 2
@@ -85,27 +86,30 @@ def find_neg_closest(dhs_midpoint, features):
 
 def get_closest_gene(ref_genome, chrom_name, start, end):
     range_midpoint = (start + end) // 2
-    closest_pos_feature = find_pos_closest(range_midpoint, get_pos_features(chrom_name, ref_genome))
 
-    closest_neg_feature = find_neg_closest(range_midpoint, get_neg_features(chrom_name, ref_genome))
+    closest_pos_gene = find_pos_closest(range_midpoint, get_pos_genes(chrom_name, ref_genome))
+    pos_feature_distance = (
+        range_midpoint - closest_pos_gene["location"].lower if closest_pos_gene is not None else sys.maxsize
+    )
+    closest_neg_gene = find_neg_closest(range_midpoint, get_neg_genes(chrom_name, ref_genome))
+    neg_feature_distance = (
+        range_midpoint - closest_neg_gene["location"].upper if closest_neg_gene is not None else sys.maxsize
+    )
 
-    if closest_pos_feature is None and closest_neg_feature is None:
-        closest_feature = None
-        distance = -1
-        gene_name = "No Gene"
-    elif closest_pos_feature is None:
-        closest_feature = closest_neg_feature
-        distance = abs(range_midpoint - closest_neg_feature["location"].upper)
-    elif closest_neg_feature is None or abs(range_midpoint - closest_pos_feature["location"].lower) <= abs(
-        closest_neg_feature["location"].upper - range_midpoint
-    ):
-        closest_feature = closest_pos_feature
-        distance = abs(range_midpoint - closest_pos_feature["location"].lower)
-    else:
-        closest_feature = closest_neg_feature
-        distance = abs(closest_neg_feature["location"].upper - range_midpoint)
+    closest_gene = None
+    distance = None
+    gene_name = "No Gene"
 
-    if closest_feature is not None:
-        gene_name = closest_feature["name"]
+    if closest_pos_gene is None and closest_neg_gene is None:
+        pass
+    elif closest_pos_gene is None or abs(neg_feature_distance) < abs(pos_feature_distance):
+        closest_gene = closest_neg_gene
+        distance = neg_feature_distance
+    elif closest_neg_gene is None or abs(pos_feature_distance) <= abs(neg_feature_distance):
+        closest_gene = closest_pos_gene
+        distance = pos_feature_distance
 
-    return closest_feature, distance, gene_name
+    if closest_gene is not None:
+        gene_name = closest_gene["name"]
+
+    return closest_gene, distance, gene_name
